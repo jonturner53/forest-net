26284ba8dc3e511df15dcb3157932cceff045edb
jonturneratwustl@gmail.com <jonturneratwustl@gmail.com@859b52aa-9282-db45-41ff-2f5699c6ee4d>
2011-05-21 17:33:52 +0000

    updating UiList

--- trunk/support/UiList.cpp
+++ trunk/support/UiList.cpp
@@ -2,9 +2,9 @@
 
 #include "UiList.h"
 
-/** Create a list that can hold items in 1..n1 */
-UiList::UiList(int n1) : n(n1) {
-	assert(n >= 0); makeSpace();
+/** Create a list that can hold items in 1..nn1 */
+UiList::UiList(int nn1) : nn(nn1) {
+	assert(nn >= 0); makeSpace();
 	first = last = 0;
 }
 
@@ -14,13 +14,13 @@ UiList::~UiList() { freeSpace(); }
  *  Assumes n has been initialized.
  */
 void UiList::makeSpace() {
-	next = new item[n+1];
-	for (item i = 1; i <= n; i++) nxt[i] = -1;
+	nxt = new item[n()+1];
+	for (item i = 1; i <= n(); i++) nxt[i] = -1;
 	nxt[0] = 0;
 }
 
 /** Free dynamic storage used by list. */
-void UiList::freeSpace() { delete [] next; }
+void UiList::freeSpace() { delete [] nxt; }
 
 /** Get an item based on its position in the list.
  *  @param i is position of item to be returned
@@ -40,8 +40,8 @@ item UiList::get(int i) const {
  *  if zero, i is inserted at the front of the list
  *  @return true if list was modified, else false
  */
-void UiList::insert(item i, item j) {
-	assert((i == 0 || valid(i)) && (j == 0 || valid(j));
+bool UiList::insert(item i, item j) {
+	assert((i == 0 || valid(i)) && (j == 0 || valid(j)));
 	if (i == 0 || member(i)) return false;
 	if (j == 0) {
 		if (empty()) last = i;
@@ -52,19 +52,6 @@ void UiList::insert(item i, item j) {
 	if (last == j) last = i;
 	return true;
 }
-
-
-/** Add item to the front of the list.
- *  @param item to be added.
- *  @return true if the list was modified, else false
- */
-bool UiList::addFirst(item i) { return insert(i,0); }
-
-/** Add item to the end of the list.
- *  @param item to be added.
- *  @return true if the list was modified, else false
- */
-bool UiList::addLast(item i) { return insert(i,tail); }
 
 /** Remove the item following a specified item.
  *  @param i is item whose successor is to be removed;
@@ -82,23 +69,18 @@ bool UiList::removeNext(item i) {
 	return true;
 }
 
-/** Remove the first item in the list.
- *  @return true if the list was modified, else false
- */
-bool UiList::removeFirst() { return removeNext(0); }
-
 /** Copy into list from src. */
 void UiList::copyFrom(const UiList& src) {
-	if (src.n > n) {
-		freeSpace(); n = src.n; makeSpace();
+	if (src.n() > n()) {
+		freeSpace(); nn = src.n(); makeSpace();
 	}
 	item i;
-	for (i = 1; i <= src.n; i++) nxt[i] = src.nxt[i];
-	for (i = src.n+1; i <= n; i++) nxt[i] = -1;
+	for (i = 1; i <= src.n(); i++) nxt[i] = src.nxt[i];
+	for (i = src.n()+1; i <= n(); i++) nxt[i] = -1;
 	first = src.first; last = src.last;
 }
 
-// Remove all elements from list.
+/** Remove all elements from list. */
 void UiList::clear() {
 	while (first != 0) {
 		item i = first; first = nxt[i]; nxt[i] = -1;
@@ -106,10 +88,10 @@ void UiList::clear() {
 	last = 0;
 }
 
-// Print the contents of the list.
-ostream& operator<<(ostream& os, const UiList& L) {
-	for (item i = L.first; i != Null; i = L.nxt[i]) {
-		Misc::writeNode(os,i,L.N); os << ' ';
-	}
-	return os;
+/** Write the contents of the list. */
+void UiList::write(ostream& os) {
+	os << "[ ";
+	for (item i = head(); i != 0; i = next(i))
+		Misc::writeNode(os,i,n()); os << ' ';
+	os << "]";
 }
--- trunk/support/UiList.h
+++ trunk/support/UiList.h
@@ -25,7 +25,7 @@ public:		UiList(int=26);
 	item	next(item) const;
 	item	head() const;
 	item	tail() const;
-	item	max() const;
+	item	n() const;
 
 	// predicates
 	bool	valid(item) const;
@@ -37,49 +37,66 @@ public:		UiList(int=26);
 	bool	removeNext(item);
 	bool	addFirst(item);
 	bool	addLast(item);
-	bool	removeFirst(item);
+	bool	removeFirst();
 	void	copyFrom(const UiList&);
 	void	clear();
 
 	// input/output
 	// bool	read(istream&);
-	bool	write(ostream&);
+	void	write(ostream&);
 
 protected:
-	int	n;			// list defined on ints in {1,...,n}
-	item	first;			// first item in list
-	item	last;			// last UiList in UiList
-	item	*nxt;			// nxt[i] is successor of i in list
+	// managing dynamic storage
+        void    makeSpace();   
+	void	freeSpace();
 
-	// managing storage in context of inheritance
-        void    virtual makeSpace();   
-	void	virtual freeSpace();
-        void    virtual copyFrom(const UiList&);
+private:
+	int	nn;			///< list defined on ints in {1,...,nn}
+	item	first;			///< first item in list
+	item	last;			///< last UiList in UiList
+	item	*nxt;			///< nxt[i] is successor of i in list
 };
 
 /** Return the successor of i. */
-inline item list::suc(item i) const {
-        assert(valid(i)); return next[i];
+inline item UiList::next(item i) const {
+        assert(valid(i)); return nxt[i];
 }
-
-/** Return last item on list. */
-inline item UiList::tail() const { return last; }
 
 /** Return first item on list. */
 inline item UiList::head() const { return first; }
 
+/** Return last item on list. */
+inline item UiList::tail() const { return last; }
+
 /** Return the largest value that can be stored in list. */
-inline item UiList::max() const { return n; }
+inline item UiList::n() const { return nn; }
 
 /** Test if a given item is valid for this UiList */
-inline bool UiList::valid(item i) { return 1 <= i && i <= N; }
+inline bool UiList::valid(item i) const { return 1 <= i && i <= n(); }
 
 /** Return true if list is empty. */
 inline bool UiList::empty() const { return first == 0; }
 
 /** Return true if specified item in list, else false. */
 inline bool UiList::member(item i) const {
-	return valid(i) && next[i] != -1;
+	return valid(i) && next(i) != -1;
 }
+
+/** Add item to the front of the list.
+ *  @param item to be added.
+ *  @return true if the list was modified, else false
+ */
+bool UiList::addFirst(item i) { return insert(i,0); }
+
+/** Add item to the end of the list.
+ *  @param item to be added.
+ *  @return true if the list was modified, else false
+ */
+bool UiList::addLast(item i) { return insert(i,tail()); }
+
+/** Remove the first item in the list.
+ *  @return true if the list was modified, else false
+ */
+bool UiList::removeFirst() { return removeNext(0); }
 
 #endif
