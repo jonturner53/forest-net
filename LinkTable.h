be3caaab00e90f5067fade9e4c521f50b50a1086
jonturneratwustl@gmail.com <jonturneratwustl@gmail.com@859b52aa-9282-db45-41ff-2f5699c6ee4d>
2011-08-28 17:03:29 +0000

    adding more source documentation

--- LinkTable.h
+++ LinkTable.h
@@ -91,100 +91,282 @@ private:
 	void	writeEntry(ostream&, int) const;
 };
 
-inline bool LinkTable::valid(int i) const { return links->isIn(i); }
+/** Determine if a link number is valid.
+ *  @param lnk is a link number
+ *  @return true if a link has been defined with the specified
+ *  link number, else false
+ */
+inline bool LinkTable::valid(int lnk) const { return links->isIn(lnk); }
 
+/** Get the "first" link.
+ *  This method is used to iterate through the links.
+ *  The order of the links is arbitrary.
+ *  @return the link number of the first link, or 0 if there are no links
+ */
 inline int LinkTable::firstLink() const {
 	return links->firstIn();
 }
 
+/** Get the "next" link.
+ *  This method is used to iterate through the links.
+ *  The order of the links is arbitrary.
+ *  @param lnk is a link number
+ *  @return the link number of the link following lnk,
+ *  or 0 if there are no more links
+ */
 inline int LinkTable::nextLink(int lnk) const {
 	return links->nextIn(lnk);
 }
 
-// getters
-inline ipa_t LinkTable::getPeerIpAdr(int i) const { return lnkTbl[i].peerIp; }
-inline ipp_t LinkTable::getPeerPort(int i) const { return lnkTbl[i].peerPort; }
-inline int LinkTable::getIface(int i) const { return lnkTbl[i].iface; }
-inline ntyp_t LinkTable::getPeerType(int i) const { return lnkTbl[i].peerType; }
-inline fAdr_t LinkTable::getPeerAdr(int i) const { return lnkTbl[i].peerAdr; }
-inline int LinkTable::getBitRate(int i) const { return lnkTbl[i].bitRate; }
-inline int LinkTable::getPktRate(int i) const { return lnkTbl[i].pktRate; }
-
-inline int LinkTable::getAvailInBitRate(int i) const {
-	return lnkTbl[i].avInBitRate;
-}
-inline int LinkTable::getAvailInPktRate(int i) const {
-	return lnkTbl[i].avInPktRate;
-}
-inline int LinkTable::getAvailOutBitRate(int i) const {
-	return lnkTbl[i].avOutBitRate;
-}
-inline int LinkTable::getAvailOutPktRate(int i) const {
-	return lnkTbl[i].avOutPktRate;
+/** Get the IP address of the peer for a given link.
+ *  @param lnk is a valid link number
+ *  @return the IP address for the far end of the link
+ */
+inline ipa_t LinkTable::getPeerIpAdr(int lnk) const {
+	return lnkTbl[lnk].peerIp;
 }
 
-// setters
-inline void LinkTable::setIface(int i, int iface) { lnkTbl[i].iface = iface; }
-inline void LinkTable::setPeerType(int i, ntyp_t nt) { lnkTbl[i].peerType =nt; }
-inline void LinkTable::setPeerAdr(int i, fAdr_t adr) { lnkTbl[i].peerAdr =adr; }
-inline void LinkTable::setBitRate(int i, int br) { lnkTbl[i].bitRate = br; }
-inline void LinkTable::setPktRate(int i, int pr) { lnkTbl[i].pktRate = pr; }
-
-inline bool LinkTable::setAvailInBitRate(int i, int br) {
-	if (br > lnkTbl[i].bitRate) return false;
-	lnkTbl[i].avInBitRate = max(0,br);
-	return true;
-}
-inline bool LinkTable::setAvailInPktRate(int i, int pr) {
-	if (pr > lnkTbl[i].pktRate) return false;
-	lnkTbl[i].avInPktRate = max(0,pr);
-	return true;
-}
-inline bool LinkTable::setAvailOutBitRate(int i, int br) {
-	if (br > lnkTbl[i].bitRate) return false;
-	lnkTbl[i].avOutBitRate = max(0,br);
-	return true;
-}
-inline bool LinkTable::setAvailOutPktRate(int i, int pr) {
-	if (pr > lnkTbl[i].pktRate) return false;
-	lnkTbl[i].avOutPktRate = max(0,pr);
-	return true;
-}
-inline bool LinkTable::addAvailInBitRate(int i, int br) {
-	int s = br + lnkTbl[i].avInBitRate;
-	if (s > lnkTbl[i].bitRate) return false;
-	lnkTbl[i].avInBitRate = max(0,s);
-	return true;
-}
-inline bool LinkTable::addAvailInPktRate(int i, int pr) {
-	int s = pr + lnkTbl[i].avInPktRate;
-	if (s > lnkTbl[i].pktRate) return false;
-	lnkTbl[i].avInPktRate = max(0,s);
-	return true;
-}
-inline bool LinkTable::addAvailOutBitRate(int i, int br) {
-	int s = br + lnkTbl[i].avOutBitRate;
-	if (s > lnkTbl[i].bitRate) return false;
-	lnkTbl[i].avOutBitRate = max(0,s);
-	return true;
-}
-inline bool LinkTable::addAvailOutPktRate(int i, int pr) {
-	int s = pr + lnkTbl[i].avOutPktRate;
-	if (s > lnkTbl[i].pktRate) return false;
-	lnkTbl[i].avOutPktRate = max(0,s);
-	return true;
+/** Get the port number of the peer for a given link.
+ *  @param lnk is a valid link number
+ *  @return the port number for the far end of the link
+ */
+inline ipp_t LinkTable::getPeerPort(int lnk) const {
+	return lnkTbl[lnk].peerPort;
 }
 
+/** Get the interface number for a given link.
+ *  @param lnk is a valid link number
+ *  @return the number of the interface that the link is assigned to
+ */
+inline int LinkTable::getIface(int lnk) const {
+	return lnkTbl[lnk].iface;
+}
 
-/** Compute key for hash lookup
+/** Get the node type of the peer for a given link.
+ *  @param lnk is a valid link number
+ *  @return the type of the node at the far end of the link
+ */
+inline ntyp_t LinkTable::getPeerType(int lnk) const {
+	return lnkTbl[lnk].peerType;
+}
+
+/** Get the Forest address of the peer for a given link.
+ *  @param lnk is a valid link number
+ *  @return the Forest address for the far end of the link
+ */
+inline fAdr_t LinkTable::getPeerAdr(int lnk) const {
+	return lnkTbl[lnk].peerAdr;
+}
+
+/** Get the bit rate of a given link.
+ *  @param lnk is a valid link number
+ *  @return the bit rate for the link
+ */
+inline int LinkTable::getBitRate(int lnk) const {
+	return lnkTbl[lnk].bitRate;
+}
+
+/** Get the packet rate of a given link.
+ *  @param lnk is a valid link number
+ *  @return the packet rate for the link
+ */
+inline int LinkTable::getPktRate(int lnk) const {
+	return lnkTbl[lnk].pktRate;
+}
+
+/** Get the available incoming bit rate of a given link.
+ *  @param lnk is a valid link number
+ *  @return the available incoming bit rate for the link
+ */
+inline int LinkTable::getAvailInBitRate(int lnk) const {
+	return lnkTbl[lnk].avInBitRate;
+}
+
+/** Get the available incoming packet rate of a given link.
+ *  @param lnk is a valid link number
+ *  @return the available incoming packet rate for the link
+ */
+inline int LinkTable::getAvailInPktRate(int lnk) const {
+	return lnkTbl[lnk].avInPktRate;
+}
+
+/** Get the available outcoming bit rate of a given link.
+ *  @param lnk is a valid link number
+ *  @return the available outcoming bit rate for the link
+ */
+inline int LinkTable::getAvailOutBitRate(int lnk) const {
+	return lnkTbl[lnk].avOutBitRate;
+}
+
+/** Get the available outcoming bit rate of a given link.
+ *  @param lnk is a valid link number
+ *  @return the available outcoming bit rate for the link
+ */
+inline int LinkTable::getAvailOutPktRate(int lnk) const {
+	return lnkTbl[lnk].avOutPktRate;
+}
+
+/** Set the interface number for a given link.
+ *  @param lnk is a valid link number
+ *  @param iface is an interface number
+ */
+inline void LinkTable::setIface(int lnk, int iface) {
+	if (valid(lnk)) lnkTbl[lnk].iface = iface;
+}
+
+/** Set the type of the peer for a given link.
+ *  @param lnk is a valid link number
+ *  @param nt is a node type
+ */
+inline void LinkTable::setPeerType(int lnk, ntyp_t nt) {
+	if (valid(lnk)) lnkTbl[lnk].peerType = nt;
+}
+
+/** Set the Forest address of the peer for a given link.
+ *  @param lnk is a valid link number
+ *  @param adr is a Forest unicast address
+ */
+inline void LinkTable::setPeerAdr(int lnk, fAdr_t adr) {
+	if (valid(lnk)) lnkTbl[lnk].peerAdr =adr;
+}
+
+/** Set the bit rate for a given link.
+ *  If the specified rate is outside the allowed range, it is set
+ *  to the endpoint of the range.
+ *  @param lnk is a valid link number
+ *  @param br is the new bit rate
+ */
+inline void LinkTable::setBitRate(int lnk, int br) {
+	if (valid(lnk))
+		lnkTbl[lnk].bitRate = min(Forest::MAXBITRATE,
+				      max(Forest::MINBITRATE,br));
+}
+
+/** Set the packet rate for a given link.
+ *  If the specified rate is outside the allowed range, it is set
+ *  to the endpoint of the range.
+ *  @param lnk is a valid link number
+ *  @param pr is the new packet rate
+ */
+inline void LinkTable::setPktRate(int lnk, int pr) {
+	if (valid(lnk))
+		lnkTbl[lnk].pktRate = min(Forest::MAXPKTRATE,
+				      max(Forest::MINPKTRATE,pr));
+}
+
+/** Set the available incoming bit rate for a link.
+ *  @param lnk is a valid link number
+ *  @param br is the new available bit rate in the input direction
+ *  @return true on success, false on failure
+ */
+inline bool LinkTable::setAvailInBitRate(int lnk, int br) {
+	if (!valid(lnk) || br > lnkTbl[lnk].bitRate) return false;
+	lnkTbl[lnk].avInBitRate = max(0,br);
+	return true;
+}
+
+/** Set the available incoming packet rate for a link.
+ *  @param lnk is a valid link number
+ *  @param br is the new available packet rate in the input direction
+ *  @return true on success, false on failure
+ */
+inline bool LinkTable::setAvailInPktRate(int lnk, int pr) {
+	if (!valid(lnk) || pr > lnkTbl[lnk].pktRate) return false;
+	lnkTbl[lnk].avInPktRate = max(0,pr);
+	return true;
+}
+
+/** Set the available outgoing bit rate for a link.
+ *  @param lnk is a valid link number
+ *  @param br is the new available bit rate in the output direction
+ *  @return true on success, false on failure
+ */
+inline bool LinkTable::setAvailOutBitRate(int lnk, int br) {
+	if (!valid(lnk) || br > lnkTbl[lnk].bitRate) return false;
+	lnkTbl[lnk].avOutBitRate = max(0,br);
+	return true;
+}
+
+/** Set the available outgoing packet rate for a link.
+ *  @param lnk is a valid link number
+ *  @param pr is the new available packet rate in the output direction
+ *  @return true on success, false on failure
+ */
+inline bool LinkTable::setAvailOutPktRate(int lnk, int pr) {
+	if (!valid(lnk) || pr > lnkTbl[lnk].pktRate) return false;
+	lnkTbl[lnk].avOutPktRate = max(0,pr);
+	return true;
+}
+
+/** Add to the available incoming bit rate for a link.
+ *  @param lnk is a valid link number
+ *  @param br is the amount to be added to the available bit rate
+ *  in the input direction
+ *  @return true on success, false on failure
+ */
+inline bool LinkTable::addAvailInBitRate(int lnk, int br) {
+	if (!valid(lnk)) return false;
+	int s = br + lnkTbl[lnk].avInBitRate;
+	if (s > lnkTbl[lnk].bitRate) return false;
+	lnkTbl[lnk].avInBitRate = max(0,s);
+	return true;
+}
+
+/** Add to the available incoming packet rate for a link.
+ *  @param lnk is a valid link number
+ *  @param pr is the amount to be added to the available packet rate
+ *  in the output direction
+ *  @return true on success, false on failure
+ */
+inline bool LinkTable::addAvailInPktRate(int lnk, int pr) {
+	if (!valid(lnk)) return false;
+	int s = pr + lnkTbl[lnk].avInPktRate;
+	if (s > lnkTbl[lnk].pktRate) return false;
+	lnkTbl[lnk].avInPktRate = max(0,s);
+	return true;
+}
+
+/** Add to the available outgoing bit rate for a link.
+ *  @param lnk is a valid link number
+ *  @param br is the amount to be added to the available bit rate
+ *  in the input direction
+ *  @return true on success, false on failure
+ */
+inline bool LinkTable::addAvailOutBitRate(int lnk, int br) {
+	if (!valid(lnk)) return false;
+	int s = br + lnkTbl[lnk].avOutBitRate;
+	if (s > lnkTbl[lnk].bitRate) return false;
+	lnkTbl[lnk].avOutBitRate = max(0,s);
+	return true;
+}
+
+/** Add to the available outgoing packet rate for a link.
+ *  @param lnk is a valid link number
+ *  @param br is the amount to be added to the available packet rate
+ *  in the output direction
+ *  @return true on success, false on failure
+ */
+inline bool LinkTable::addAvailOutPktRate(int lnk, int pr) {
+	if (!valid(lnk)) return false;
+	int s = pr + lnkTbl[lnk].avOutPktRate;
+	if (s > lnkTbl[lnk].pktRate) return false;
+	lnkTbl[lnk].avOutPktRate = max(0,s);
+	return true;
+}
+
+/** Compute key for hash lookup.
+ *  @param ipa is an IP address
+ *  @param ipp is an IP port number
  */
 inline uint64_t LinkTable::hashkey(ipa_t ipa, ipp_t ipp) const {
 	return (((uint64_t) ipa) << 32) | ((uint64_t) ipp);
 }
 
 /** Get the link number on which a packet arrived.
- *  @param h is a reference to a PacketHeader for an incoming packet
- *  @return the link number that matches h or 0 if none match
+ *  @param ipa is an IP address
+ *  @param ipp is an IP port number
+ *  @return the number of the link that matches the given IP address
+ *  and port, or 0 if there is no matching link
  */
 inline int LinkTable::lookup(ipa_t ipa, ipp_t ipp) const {
         return ht->lookup(hashkey(ipa,ipp));
