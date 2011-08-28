512173b2ed92c15b943b8e1a326719dc9aa3ce06
jonturneratwustl@gmail.com <jonturneratwustl@gmail.com@859b52aa-9282-db45-41ff-2f5699c6ee4d>
2011-08-28 17:04:04 +0000

    adding more source documentation

--- trunk/LinkTable.cpp
+++ trunk/LinkTable.cpp
@@ -8,13 +8,14 @@
 
 #include "LinkTable.h"
 
-// Constructor for LinkTable, allocates space and initializes table.
+/** Constructor for LinkTable, allocates space and initializes table. */
 LinkTable::LinkTable(int maxLnk1) : maxLnk(maxLnk1) {
 	lnkTbl = new LinkInfo[maxLnk+1];
 	links = new UiSetPair(maxLnk);
 	ht = new UiHashTbl(maxLnk);
 };
 	
+/** Destructor for LinkTable, frees dynamic storage. */
 LinkTable::~LinkTable() { delete [] lnkTbl; delete links; delete ht; }
 
 /** Add a link table entry.
@@ -46,22 +47,35 @@ int LinkTable::addEntry(int lnk, ipa_t peerIp, ipp_t peerPort) {
         return lnk;
 }
 
+/** Set the port number for the peer at the far end of a link.
+ *  @param lnk is a valid link number
+ *  @param peerPort is the port number of the peer node
+ *  @return true on success, false on failure
+ */
 bool LinkTable::setPeerPort(int lnk, ipp_t peerPort) {
+	if (!valid(lnk)) return false;
 	ht->remove(hashkey(getPeerIpAdr(lnk),getPeerPort(lnk)));
 	if (!ht->insert(hashkey(getPeerIpAdr(lnk), peerPort),lnk))
+		ht->insert(hashkey(getPeerIpAdr(lnk), getPeerPort(lnk)),lnk))
 		return false;
+	}
 	lnkTbl[lnk].peerPort = peerPort;
 	return true;
 }
 
-// Remove the table entry for lnk. Return true on success, false on failure.
+/** Remove the table entry for a link.
+ *  @param lnk is the link number for the entry to be deleted
+ */
 void LinkTable::removeEntry(int lnk) {
 	if (!links->isIn(lnk)) return;
 	ht->remove(hashkey(getPeerIpAdr(lnk),getPeerPort(lnk)));
 	links->swap(lnk); 
 }
 
-// Return true if entry is consistent, else false.
+/** Check if a table entry is consistent.
+ *  @param lnk is the link number for the entry to be checked
+ *  @return true if entry is consistent, else false
+ */
 bool LinkTable::checkEntry(int lnk) {
 	// the forest address of every peer must be a valid unicast address
 	if (!Forest::validUcastAdr(getPeerAdr(lnk))) return false;
@@ -74,23 +88,23 @@ bool LinkTable::checkEntry(int lnk) {
 	return true;
 }
 
-// Read an entry from is and store it in the link table.
-// Return the link number for the new link.
-// A line is a pure comment line if it starts with a # sign.
-// A trailing comment is also allowed at the end of a line by
-// including a # sign. All lines that are not pure comment lines
-// or blank are assumed to contain a complete link table entry.
-// Each entry consists of a link#, the IP address of the local
-// endpoint, the IP address and port number of the peer,
-// the type of the peer, the forest address of the peer
-// and the allowed forest destination address for the peer.
-// Finally, the entry includes a max bit rate for the link
-// (in Kb/s) and a maximum packet rate (in p/s).
-//
-// If the link number specified in the input is already in use,
-// the call to readEntry will fail, in which case 0 is returned.
-// The call can also fail if the input is not formatted correctly.
-//
+/** Read an entry from an input stream and store it in the link table.
+ *  Each entry must be on its own line, possibly followed by a comment.
+ *  A comment begins with a # sign and continues to the end of the link.
+ *  A non-blank line that does not being with a comment is assumed to
+ *  contain an entry.
+ *  Each entry consists of a link#, the IP address of the local
+ *  endpoint, the IP address and port number of the peer,
+ *  the type of the peer, the forest address of the peer,
+ *  the maximum bit rate for the link (in Kb/s) and 
+ *  its maximum packet rate (in p/s).
+ * 
+ *  If the link number specified in the input is already in use,
+ *  the call to readEntry will fail, in which case 0 is returned.
+ *  The call can also fail if the input is not formatted correctly.
+ *  @param in is an open input stream
+ *  @return the link number of the entry, or 0 if the operation fails
+ */
 int LinkTable::readEntry(istream& in) {
 	int lnk, iface, bRate, pRate;
 	ipa_t peerIp; ipp_t peerPort;
@@ -123,10 +137,16 @@ int LinkTable::readEntry(istream& in) {
 	return lnk;
 }
 
-// Read link table entries from the input. The first line must contain an
-// integer, giving the number of entries to be read. The input may
-// include blank lines and comment lines (any text starting with '#').
-// Each entry must be on a line by itself (possibly with a trailing comment).
+/** Read link table entries from the input.
+ *  The first line must contain an
+ *  integer, giving the number of entries to be read. The input may
+ *  include blank lines and comment lines (any text starting with '#').
+ *  Each entry must be on a line by itself (possibly with a trailing comment).
+ *  If an error is encountered, a message is sent to cerr identifying the
+ *  the input entry that caused the error.
+ *  @param in is an open input stream
+ *  @return true on success, false on failure
+ */
 bool LinkTable::read(istream& in) {
 	int num;
  	Misc::skipBlank(in);
@@ -134,32 +154,39 @@ bool LinkTable::read(istream& in) {
         Misc::cflush(in,'\n');
 	for (int i = 1; i <= num; i++) {
 		if (readEntry(in) == 0) {
-			cerr << "Error reading link table entry # "
-			     << i << endl;
+			cerr << "LinkTable::read: could not read "
+			     << i << "-th table entry" << endl;
 			return false;
 		}
 	}
 	return true;
 }
 
-void LinkTable::writeEntry(ostream& out, int i) const {
-// Print entry for link i
-	out << setw(5) << right << i << setw(6) << getIface(i) << "  ";
+/** Write a single entry to an output stream.
+ *  @param out is an open output stream
+ *  @param lnk is the number of the link to be written out
+ */
+void LinkTable::writeEntry(ostream& out, int lnk) const {
+	if (!valid(lnk)) return;
+
+	out << setw(5) << right << lnk << setw(6) << getIface(lnk) << "  ";
 	
 	string s;
-	out << setw(12) << Np4d::ip2string(getPeerIpAdr(i),s)
-	    << ":" << setw(5) << left << getPeerPort(i) << "  ";
-	out << setw(10) << left << Forest::nodeType2string(getPeerType(i),s);
+	out << setw(12) << Np4d::ip2string(getPeerIpAdr(lnk),s)
+	    << ":" << setw(5) << left << getPeerPort(lnk) << "  ";
+	out << setw(10) << left << Forest::nodeType2string(getPeerType(lnk),s);
 
-	out << " " << setw(10) << left << Forest::fAdr2string(getPeerAdr(i),s);
+	out << " " << setw(10) << left <<Forest::fAdr2string(getPeerAdr(lnk),s);
 	
-	out << " " << setw(6) << right << getBitRate(i)
-	    << " " << setw(6) << right << getPktRate(i)
+	out << " " << setw(6) << right << getBitRate(lnk)
+	    << " " << setw(6) << right << getPktRate(lnk)
 	    << endl;
 }
 
+/** Write the link table to an output stream.
+ *  @param out is an open output stream
+ */
 void LinkTable::write(ostream& out) const {
-// Output human readable representation of link table.
 	out << links->getNumIn() << endl;
 	out << "# link  iface    peerIp:port     peerType  peerAdr   ";
         out << "  bitRate pktRate\n";
