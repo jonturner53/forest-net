f9709cabaf5a250ce89c63c73836e49a2a3eee13
jonturneratwustl@gmail.com <jonturneratwustl@gmail.com@859b52aa-9282-db45-41ff-2f5699c6ee4d>
2011-08-28 17:04:56 +0000

    added IfaceTable to repository

---
+++ IfaceTable.cpp
@@ -0,0 +1,124 @@
+/** @file IfaceTable.h 
+ *
+ *  @author Jon Turner
+ *  @date 2011
+ *  This is open source software licensed under the Apache 2.0 license.
+ *  See http://www.apache.org/licenses/LICENSE-2.0 for details.
+ */
+
+#include "IfaceTable.h"
+
+/** Constructor for IfaceTable, allocates space and initializes private data.
+ */
+IfaceTable::IfaceTable(int maxIf1) : maxIf(maxIf1) {
+	ift = new IfaceInfo[maxIf+1];
+	ifaces = new UiSetPair(maxIf);
+	defaultIf = 0;
+}
+
+/** Destructor for IfaceTable, deletes allocated space */
+IfaceTable::~IfaceTable() { delete [] ift; delete ifaces; }
+
+/** Allocate and initialize a new interface table entry.
+ *  @param iface is an interface number for an unused interface
+ *  @param ipa is the IP address to be associated with the interface
+ *  @param brate is the bitrate of the interface
+ *  @param prate is the packet rate of the interface
+ *  Return true on success, false on failure
+ */
+bool IfaceTable::addEntry(int iface, ipa_t ipa, int brate, int prate) {
+	if (!ifaces->isOut(iface)) return false;
+	if (ifaces->firstIn() == 0) // this is the first iface
+		defaultIf = iface;
+	ifaces->swap(iface);
+	ift[iface].ipa = ipa;
+	ift[iface].maxbitrate = brate; ift[iface].maxpktrate = prate;
+	ift[iface].avbitrate = brate; ift[iface].avpktrate = prate;
+	return true;
+}
+
+/** Remove an interface from the table.
+ *  @param iface is the number of the interface to be removed
+ *  No action is taken if the specified interface number is not valid
+ */
+void IfaceTable::removeEntry(int iface) {
+	if (ifaces->isIn(iface)) ifaces->swap(iface);
+	if (iface == defaultIf) defaultIf = 0;
+}
+
+/** Read a table entry from an input stream and add it to the table.
+ *  The input stream is assumed to be positioned at the start of
+ *  an interface table entry. An entry is consists of an interface
+ *  number, an IP address followed by a colon and port number,
+ *  a maximum bit rate (in Kb/s) and a maximum packet rate (in p/s).
+ *  Each field is separated by one or more spaces.
+ *  Comments in the input stream are ignored. A comment starts with
+ *  a # sign and continues to the end of the line. Non-blank lines that do
+ *  not start with a comment are expected to contain a complete entry.
+ *  If the input is formatted incorrectly, or the interface number
+ *  specified in the input is already in use, the operation will fail.
+ *  @param in is an open input stream
+ *  @return the number of the new interface or 0, if the operation failed
+ */ 
+int IfaceTable::readEntry(istream& in) {
+	int ifnum, brate, prate; ipa_t ipa;
+
+	Misc::skipBlank(in);
+	if ( !Misc::readNum(in,ifnum) || !Np4d::readIpAdr(in,ipa) ||
+	     !Misc::readNum(in,brate) || !Misc::readNum(in,prate)) {
+		return 0;
+	}
+	Misc::cflush(in,'\n');
+
+	if (!addEntry(ifnum,ipa,brate,prate)) return 0;
+	return ifnum;
+}
+
+/** Read interface table entries from the input.
+ *  The first line must contain an
+ *  integer, giving the number of entries to be read. The input may
+ *  include blank lines and comment lines (any text starting with '#').
+ *  Each entry must be on a line by itself (possibly with a trailing comment).
+ *  If the operation fails, a message is sent to cerr, identifying the
+ *  interface that triggered the failure
+ *  @param in is an open input stream
+ *  @return true on success, false on failure
+ */
+bool IfaceTable::read(istream& in) {
+	int num;
+ 	Misc::skipBlank(in);
+        if (!Misc::readNum(in,num)) return false;
+        Misc::cflush(in,'\n');
+	for (int i = 1; i <= num; i++) {
+		if (readEntry(in) == 0) {
+			cerr << "IfaceTable::read: Error in "
+			     << i << "-th entry read from input\n";
+			return false;
+		}
+	}
+	return true;
+}
+
+/** Write an interface table entry.
+ *  @param out is an open output stream
+ *  @param iface is the number of the interface to be written
+ */
+void IfaceTable::writeEntry(ostream& out, int iface) const {
+	out << setw(5) << iface << "   ";
+	Np4d::writeIpAdr(out,ift[iface].ipa);
+	out << setw(9) << ift[iface].maxbitrate
+	    << setw(9) << ift[iface].maxpktrate
+	    << endl;
+}
+
+/** Write the complete interface table.
+ *  The output format matches the format expected by the read method.
+ *  @param out is an open output stream
+ */
+void IfaceTable::write(ostream& out) const {
+	string s;
+	out << Misc::num2string(ifaces->getNumIn(),s) << endl;
+	out << "# iface  ipAddress      bitRate  pktRate\n";
+	for (int i = firstIface(); i != 0; i = nextIface(i)) 
+		writeEntry(out,i);
+}
---
+++ IfaceTable.h
@@ -0,0 +1,233 @@
+/** @file IfaceTable.h
+ *
+ *  @author Jon Turner
+ *  @date 2011
+ *  This is open source software licensed under the Apache 2.0 license.
+ *  See http://www.apache.org/licenses/LICENSE-2.0 for details.
+ */
+
+#ifndef IFACETABLE_H
+#define IFACETABLE_H
+
+#include "CommonDefs.h"
+#include "UiSetPair.h"
+
+class IfaceTable {
+public:
+		IfaceTable(int);
+		~IfaceTable();
+
+	// predicates 
+	bool 	valid(int) const;	
+
+	// iteration methods
+	int	firstIface() const;
+	int	nextIface(int) const;
+
+	// access methods 
+	ipa_t	getIpAdr(int) const;	
+	int	getMaxBitRate(int) const;
+	int	getMaxPktRate(int) const;
+	int	getAvailBitRate(int) const;
+	int	getAvailPktRate(int) const;
+	int	getDefaultIface() const;
+	int	getFreeIface() const;
+
+	// modifiers 
+	bool	addEntry(int,ipa_t,int,int);
+	void	removeEntry(int);
+	void	setDefaultIface(int);
+	void	setMaxBitRate(int, int);
+	void	setMaxPktRate(int, int);
+	bool	setAvailBitRate(int, int);
+	bool	setAvailPktRate(int, int);
+	bool	addAvailBitRate(int, int);
+	bool	addAvailPktRate(int, int);
+
+	// io routines
+	bool	read(istream&);
+	void	write(ostream&) const;
+private:
+	int	maxIf;			///< largest interface number
+	int	defaultIf;		///< default interface
+	struct IfaceInfo {		///< information describing an iface
+	ipa_t	ipa;			///< IP address of interface
+	int	sock;			///< socket number of interface
+	int	maxbitrate;		///< max bit rate for interface (Kb/s)
+	int	maxpktrate;		///< max packet rate for interface
+	int	avbitrate;		///< max bit rate for interface (Kb/s)
+	int	avpktrate;		///< max packet rate for interface
+	};
+	IfaceInfo *ift;			///< ift[i]=data for interface i
+	UiSetPair *ifaces;		///< in-use and available iface numbers
+
+	// helper methods
+	int	readEntry(istream&);		
+	void	writeEntry(ostream&, int) const;
+	string& entry2string(int, string&) const;
+};
+
+/** Check an interface number for validity.
+ *  @param iface is the interface to be checked
+ *  @return true if iface is a valid interface number, else false
+ */
+inline bool IfaceTable::valid(int iface) const {
+	return ifaces->isIn(iface);
+}
+
+/** Get the "first" interface number.
+ *  This method used to iterate through all the interfaces.
+ *  The choice of which interface is first is arbitrary and
+ *  programs should not rely on any particular iterface being the first.
+ *  @return the interface number of the first interface, or 0 if no
+ *  interfaces have been defined for this router
+ */
+inline int IfaceTable::firstIface() const {
+	return ifaces->firstIn();
+}
+
+/** Get the "next" interface number.
+ *  This method used to iterate through all the interfaces.
+ *  The order of the interfaces is arbitrary and programs
+ *  should not rely on any particular order.
+ *  @param iface is a valid interface number
+ *  @return the next interface number following iface, or 0 if
+ *  there is no next interface, or iface is not valid
+ */
+inline int IfaceTable::nextIface(int iface) const {
+	return ifaces->nextIn(iface);
+}
+
+/** Get the number of the default interface.
+ *  At the time the first interface is added, the default interface
+ *  is initialized with the same interface number. The default
+ *  interface can be changed using the setDefaultIface() method.
+ *  @return the interface number of the default interface, or 0
+ *  if there is no default interface
+ */
+inline int IfaceTable::getDefaultIface() const {
+	return defaultIf;
+}
+
+/** Get an an unused interface number.
+ *  @return the number of an interface that is not yet in use, or 0
+ *  if there are no available interface numbers
+ */
+inline int IfaceTable::getFreeIface() const {
+	return ifaces->firstOut();
+}
+
+/** Get the IP address associated with a given interface.
+ *  @param iface is the interface number
+ *  @return the associated IP address
+ */
+inline ipa_t IfaceTable::getIpAdr(int iface) const { return ift[iface].ipa; }	
+
+/** Get the maximum bit rate allowed for this interface.
+ *  @param i is the interface number
+ *  @return the bit rate in Kb/s
+ */
+inline int IfaceTable::getMaxBitRate(int i) const { return ift[i].maxbitrate; }
+
+/** Get the maximum packet rate allowed for this interface.
+ *  @param i is the interface number
+ *  @return the packet rate in p/s
+ */
+inline int IfaceTable::getMaxPktRate(int i) const { return ift[i].maxpktrate; }
+
+/** Get the available bit rate for this interface.
+ *  @param i is the interface number
+ *  @return the available bit rate in Kb/s
+ */
+inline int IfaceTable::getAvailBitRate(int i) const {
+	return ift[i].avbitrate;
+}
+
+/** Get the available packet rate for this interface.
+ *  @param i is the interface number
+ *  @return the available packet rate in p/s
+ */
+inline int IfaceTable::getAvailPktRate(int i) const {
+	return ift[i].avpktrate;
+}
+
+/** Set the default interface.
+ *  @param iface is the number of a valid existing interface
+ */
+inline void IfaceTable::setDefaultIface(int iface) {
+	if (valid(iface)) defaultIf = iface;
+}
+
+/** Set the maximum bit rate for an interface.
+ *  If specified rate is outside the allowed range, the rate is set to
+ *  the endpoint of the range.
+ *  @param iface is a valid interface number
+ *  @param r is the max bit rate in Kb/s
+ */
+inline void IfaceTable::setMaxBitRate(int iface, int r) {
+	ift[iface].maxbitrate = min(max(r,Forest::MINBITRATE),
+					  Forest::MAXBITRATE);
+}
+
+/** Set the maximum packet rate for an interface.
+ *  If specified rate is outside the allowed range, the rate is set to
+ *  the endpoint of the range.
+ *  @param iface is a valid interface number
+ *  @param r is the max packet rate in Kb/s
+ */
+inline void IfaceTable::setMaxPktRate(int iface, int r) {
+	ift[iface].maxpktrate = min(max(r,Forest::MINPKTRATE),
+					  Forest::MAXPKTRATE);
+}
+
+/** Set the available bit rate for this interface.
+ *  Operation fails if one attempts to over-allocate the interface.
+ *  @param iface is the interface number
+ *  @param r is the available bit rate in Kb/s
+ *  @return true on success, false on failure
+ */
+inline bool IfaceTable::setAvailBitRate(int iface, int r) {
+	if (r > ift[iface].maxbitrate) return false;
+	ift[iface].avbitrate = min(0,r);
+	return true;
+}
+
+/** Set the available packet rate for this interface.
+ *  Operation fails if one attempts to over-allocate the interface.
+ *  @param i is the interface number
+ *  @param r is the available packet rate in p/s
+ *  @return true on success, false on failure
+ */
+inline bool IfaceTable::setAvailPktRate(int iface, int r) {
+	if (r > ift[iface].maxpktrate) return false;
+	ift[iface].avpktrate = min(0,r);
+	return true;
+}
+
+/** Add to the available bit rate for this interface.
+ *  Operation fails if one attempts to over-allocate the interface.
+ *  @param iface is the interface number
+ *  @param r is the amount to add in Kb/s
+ *  @return true on success, false on failure
+ */
+inline bool IfaceTable::addAvailBitRate(int iface, int r) {
+	int s = r + ift[iface].avbitrate;
+	if (s > ift[iface].maxbitrate) return false;
+	ift[iface].avbitrate = min(0,s);
+	return true;
+}
+
+/** Add to the available packet rate for this interface.
+ *  Operation fails if one attempts to over-allocate the interface.
+ *  @param iface is the interface number
+ *  @param r is the amount to add in p/s
+ *  @return true on success, false on failure
+ */
+inline bool IfaceTable::addAvailPktRate(int iface, int r) {
+	int s = r + ift[iface].avpktrate;
+	if (s > ift[iface].maxpktrate) return false;
+	ift[iface].avpktrate = min(0,s);
+	return true;
+}
+
+#endif
