670a4a679ede2b2dcfd838711e7e8c9ae649be02
jonturneratwustl@gmail.com <jonturneratwustl@gmail.com@859b52aa-9282-db45-41ff-2f5699c6ee4d>
2011-09-05 16:36:38 +0000

    adding Queue to support library

---
+++ trunk/support/Queue.h
@@ -0,0 +1,49 @@
+/** \file Queue.h
+ *
+ *  @author Jon Turner
+ *  @date 2011
+ *  This is open source software licensed under the Apache 2.0 license.
+ *  See http://www.apache.org/licenses/LICENSE-2.0 for details.
+ */
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <pthread.h>
+#include "stdinc.h"
+#include "Misc.h"
+
+/** This class implements a simple thread-safe queue for communication
+ *  between two threads. It assumes a single reader and a single writer.
+ */
+class Queue {
+public:		Queue(int=10);
+		~Queue();
+	bool	init();
+	void	reset();
+
+	bool	empty() const;
+
+	void	enq(int);
+	int	deq();	
+	int	deq(uint32_t);	
+private:
+	int	qMax;			///< max number of items in queue
+
+	int	count;			///< number of items in queue
+	int	head;			///< index of first item in buf
+	int 	tail;			///< index of first empty space in buf
+
+	int	*buf;			///< where values are stored
+
+	pthread_mutex_t lock;		///< used to ensure mutual exclusion
+	pthread_cond_t emptyQ;		///< condition variable for empty queue
+	pthread_cond_t fullQ;		///< condition variable for full queue
+};
+
+/** Determine if queue is empty.
+ *  @return true if the queue is empty, else false
+ */
+inline bool Queue::empty() const { return count == 0; }
+
+#endif
