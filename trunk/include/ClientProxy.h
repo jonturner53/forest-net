e40587c36c4f6568f9c9434b992c46073f254d71
loganstafman@gmail.com <loganstafman@gmail.com@859b52aa-9282-db45-41ff-2f5699c6ee4d>
2012-06-22 14:16:22 +0000

    Adding clientproxy's header file

---
+++ trunk/include/ClientProxy.h
@@ -0,0 +1,33 @@
+#ifndef CLIPROXY_H
+#define CLIPROXY_H
+
+#include "CommonDefs.h"
+#include "PacketHeader.h"
+#include "PacketStore.h"
+class ClientProxy {
+public:
+	ClientProxy(ipa_t);
+	~ClientProxy();
+
+	bool init(ipa_t);
+	void run(uint32_t);
+
+	int recvFromForest();
+	int recvFromAvatar();
+	void send2cli(int);
+	void send(int);	
+private:
+	static const int LISTEN_PORT = 30140;
+	static const int UPDATE_PERIOD = 50;
+	static const int npkts = 1000;
+	PacketStore *ps;
+	ipa_t myIpAdr;
+	ipa_t rtrIp;
+	fAdr_t rtrAdr;
+	ipa_t avIp;
+	ipp_t avPort;
+	int sock;
+	int extSock;
+	int avaSock;
+};
+#endif
