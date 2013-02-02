15a01f399c385bd8c1ec31ba23c9250d0fb1d532
jonturneratwustl@gmail.com <jonturneratwustl@gmail.com@859b52aa-9282-db45-41ff-2f5699c6ee4d>
2013-02-02 02:59:31 +0000

    more bug fixes to python avatar

--- python/vworld1/WallsWorld.py
+++ python/vworld1/WallsWorld.py
@@ -20,6 +20,7 @@ class WallsWorld :
 		y = 0;
 		lines = f.readlines()
 		self.size = len(lines)
+		print "self.size=", self.size
 		y = self.size - 1
 		self.walls = [0] * (self.size*self.size)
 		for line in lines :
@@ -90,6 +91,7 @@ class WallsWorld :
 	 	"""
 		x1 = c1%self.size; y1 = c1/self.size;
 		vSet = set();
+		vSet.add(c1)
 	
 		visVec = [False] * self.size
 		prevVisVec = [False] * self.size
@@ -226,76 +228,32 @@ class WallsWorld :
 					return False
 			return True
 
-		# l1 is a line connecting the given pair of points
-		l1 = (p1,p2)
 		slope = (p2[1]-p1[1])/(p2[0]-p1[0])
 
 		i = i1; j = j1;
 		x = p1[0]; y = p1[1]
 		xd = i1+1 - x; yd = xd*slope
+		c = i+j*self.size
 		while i < i2 :
-			while j+1 < y+yd :
-				if self.walls[i+j*self.size]&2 :
-					return False
-				j += 1
-			if self.walls[(i+1)+j*self.size]&1 :
-				return False
-			while j+1 < y+slope :
-				c = (i+1)+j*self.size
-				if c < 0 or c >= len(self.walls) :
-					print "bad pair i=",i,"j=",j,\
-						"size=",self.size,\
-						"len=",len(self.walls)
-				if self.walls[c]&2 :
-					return False
-				j += 1
+			if slope > 0 :
+				while j+1 < y+yd :
+					if self.walls[c]&2 : return False
+					j += 1; c += self.size
+			else :
+				while j > y+yd :
+					j -= 1; c -= self.size
+					if self.walls[c]&2 : return False
+			c += 1
+			if self.walls[c]&1 : return False
+			if slope > 0 :
+				ylim = min(y+slope, p2[1])
+				while j+1 < ylim :
+					if self.walls[c]&2 : return False
+					j += 1; c += self.size
+			else :
+				ylim = max(y+slope, p2[1])
+				while j > ylim :
+					j -= 1; c -= self.size
+					if self.walls[c]&2 : return False
 			i += 1; x += 1.0; y += slope
 		return True
-	
-	def linesIntersect(self,l1,l2) :
-		epsilon = .001; # two lines are considered vertical if
-				# x values differ by less than epsilon
-
-		# unpack coordinates of line endpoints
-		pa = l1[0]; pb = l1[1]
-		ax = pa[0]; ay = pa[1]; bx = pb[0]; by = pb[1]
-
-		pc = l2[0]; pd = l2[1]
-		cx = pc[0]; cy = pc[1]; dx = pd[0]; dy = pd[1]
-
-		# first handle special case of two vertical lines
-		if abs(ax-bx) < epsilon and abs(cx-dx) < epsilon :
-			return	abs(ax-cx) < epsilon and \
-				max(ay,by) >= min(cy,dy) and \
-			      	min(ay,by) <= max(cy,dy)
-		# now handle cases of one vertical line
-		if abs(ax-bx) < epsilon :
-			s2 = (dy-cy)/(dx-cx)  	# slope of second line
-			i2 = cy - s2*cx  	# y-intercept of second line
-			y = s2*ax + i2	  	# lines intersect at (ax,y)
-			return  (y >= min(ay,by) and y <= max(ay,by) and \
-				 y >= min(cy,dy) and y <= max(cy,dy))
-		if abs(cx-dx) < epsilon :
-			s1 = (by-ay)/(bx-ax)	# slope of first line
-			i1 = ay - s1*ax		# y-intercept of first line
-			y = s1*cx + i1		# lines intersect at (cx,y)
-			return  (y >= min(ay,by) and y <= max(ay,by) and \
-				 y >= min(cy,dy) and y <= max(cy,dy))
-
-		s1 = (by-ay)/(bx-ax)	# slope of first line
-		i1 = ay - s1*ax 	# y-intercept of first line
-		s2 = (dy-cy)/(dx-cx)	# slope of second line
-		i2 = cy - s2*cx 	# y-intercept of second line
-	
-		# handle special case of lines with equal slope
-		# treat the slopes as equal if both slopes have very small
-		# magnitude, or if their relative difference is small
-		if abs(s1)+abs(s2) <= epsilon or \
-		   abs(s1-s2)/(abs(s1)+abs(s2)) < epsilon :
-			return (abs(i1-i2) < epsilon and \
-				min(ax,bx) <= max(cx,dx) and \
-				max(ax,bx) >= min(cx,dx))
-		# now, to the common case
-		x = (i2-i1)/(s1-s2)	# x value where the lines intersect
-		return	x >= min(ax,bx) and x <= max(ax,bx) and \
-			x >= min(cx,dx) and x <= max(cx,dx)
