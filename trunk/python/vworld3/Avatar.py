1bc038e3220f1270fed8b4ff390fa6dbc19997e5
jonturneratwustl@gmail.com <jonturneratwustl@gmail.com@859b52aa-9282-db45-41ff-2f5699c6ee4d>
2013-03-13 22:51:20 +0000

    version that eliminates dependence on WorldMap

--- trunk/python/vworld3/Avatar.py
+++ trunk/python/vworld3/Avatar.py
@@ -1,10 +1,11 @@
 """ Demonstration of simple virtual world using Forest overlay
 
 usage:
-      Avatar myIp cliMgrIp size comtree [ debug ] [ auto ]
+      Avatar myIp cliMgrIp numg maxVis comtree [ debug ] [ auto ]
 
 - myIp is the IP address of the user's computer
 - cliMgrIp is the IP address of the client manager's computer
+- numg*numg is the number of multicast groups to use
 - comtree is the number of a pre-configured Forest comtree
 - the debug option, if present controls the amount of debugging output;
   use "debug" for a little debugging output, "debuggg" for lots
@@ -14,6 +15,7 @@ usage:
 import sys
 from socket import *
 from Net import *
+from Mcast import *
 from Packet import *
 from Util import *
 from PandaWorld import *
@@ -28,35 +30,28 @@ from direct.interval.IntervalGlobal import Sequence
 from panda3d.core import Point3
 import random, sys, os, math
 
-
 # process command line arguments
-if len(sys.argv) < 5 :
-	sys.stderr.write("usage: Avatar myIp cliMgrIp size " + \
-			 "comtree [ debug ] [ auto ]\n")
+if len(sys.argv) < 6 :
+	sys.stderr.write("usage: Avatar myIp cliMgrIp numg " + \
+			 "maxVis comtree [ debug ] [ auto ]\n")
         sys.exit(1)
 
 myIp = gethostbyname(sys.argv[1])
 cliMgrIp = gethostbyname(sys.argv[2])
-size = int(sys.argv[3])
-myComtree = int(sys.argv[4])
+numg = int(sys.argv[3])
+maxVis = int(sys.argv[4])
+myComtree = int(sys.argv[5])
 
 auto = False; debug = 0
-for i in range(5,len(sys.argv)) :
+for i in range(6,len(sys.argv)) :
 	if sys.argv[i] == "debug" : debug = 1
 	elif sys.argv[i] == "debugg" : debug = 2
 	elif sys.argv[i] == "debuggg" : debug = 3
 	elif sys.argv[i] == "auto" :
 		auto = True
-#		loadPrcFile('myConfig.prc')
-#		noScreen = ConfigVariableString('window-type','none')
-
-#map = WorldMap() 
-#if not map.init(mapFile) :
-#	sys.stderr.write("cannot initialize map from mapFile\n");
-#	sys.exit(1)
 
 pWorld = None if auto else PandaWorld()
-net = Net(myIp, cliMgrIp, myComtree, size, pWorld, debug, auto)
+net = Net(myIp, cliMgrIp, myComtree, numg, maxVis, pWorld, debug, auto)
 
 # setup tasks
 if not net.init("user", "pass") :
