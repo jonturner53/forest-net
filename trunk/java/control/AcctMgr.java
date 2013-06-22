916260e690c3ce3cea6fc5d416bbf6e4f35bc57f
jonturneratwustl@gmail.com <jonturneratwustl@gmail.com@859b52aa-9282-db45-41ff-2f5699c6ee4d>
2013-06-22 19:06:52 +0000

    adding documentation to AcctMgr

--- trunk/java/control/AcctMgr.java
+++ trunk/java/control/AcctMgr.java
@@ -41,48 +41,51 @@ public class AcctMgr extends MouseAdapter implements ActionListener, FocusListen
 		});
 	}
 
-	private static int CM_PORT = 30122;
-	private SocketChannel serverChan;
-	private NetBuffer inBuf;
+	private static int CM_PORT = 30122;	///< client manager's port
+	private SocketChannel serverChan;	///< channel to client mgr
+	private NetBuffer inBuf;		///< for reading from socket
 
 	// various user interface buttons
-	private JButton connectBtn;
-	private JButton loginBtn;
-	private JButton newAcctBtn;
-	private JButton updateBtn;
-	private JButton modPwdBtn;
-	private JButton uploadBtn;
+	private JButton connectBtn;	///< connect to client mg4
+	private JButton loginBtn;	///< login to specific account
+	private JButton newAcctBtn;	///< create new account
+	private JButton updateBtn;	///< update profile
+	private JButton modPwdBtn;	///< modify password
+	private JButton uploadBtn;	///< upload photo
 
 	// various text fields and the strings use to hold contents
-	private JTextField serverField;
+	private JTextField serverField;	///< client mgr server name/ip
 	private String serverName;
-	private JTextField userField;
+	private JTextField userField;	///< user name
 	private String userName;
-	private JPasswordField pwdField;
+	private JPasswordField pwdField;///< password
 	private String password;
 
-	private JTextField nameField;
+	private JTextField nameField;	///< real name
 	private String realName;
-	private JTextField emailField;
+	private JTextField emailField;	///< email address
 	private String email;
-	private JPasswordField nupwdField;
+	private JPasswordField nupwdField; ///< new password field
 	private String newPassword;
-
-	private JTextField drateField;
-	private String defRates;
-	private JTextField trateField;
-	private String totRates;
-	private JPasswordField confirmField;
+	private JPasswordField confirmField; ///< confirm password
 	private String confirmPassword;
 
-	private JTextField updateField;
+	private JTextField drateField;	///< default rate spec (as string)
+	private String defRates;
+	private JTextField trateField;	///< total rate spec (as string)
+	private String totRates;
+
+	private JTextField updateField;	///< used to change target user
 	private String updateTarget;
-	private JTextField photoField;
+	private JTextField photoField;	///< file name of photograph
 	private String photoFile;
 
-	// text area used to display status messages
-	JTextArea statusArea;
+	JTextArea statusArea;	///< text area used to display status messages
 
+	/** Add a label to a text field and align them.
+	 *  @param text is the text field
+	 *  @param name is the text used to label the text feild
+	 */
 	private Box labeledField(JTextField text, String name) {
 		JLabel label = new JLabel(name);
 		label.setBorder(BorderFactory.createEmptyBorder(0,5,0,0));
@@ -96,6 +99,10 @@ public class AcctMgr extends MouseAdapter implements ActionListener, FocusListen
 		return box;
 	}
 		
+	/** Create a horizontal box containing a pair of boxes.
+	 *  @param b1 is the left box
+	 *  @param b2 is the right box
+	 */
 	private Box doubleBox(Box b1, Box b2) {
 		Box b = Box.createHorizontalBox();
 			   b.add(Box.createHorizontalStrut(10));
@@ -104,6 +111,11 @@ public class AcctMgr extends MouseAdapter implements ActionListener, FocusListen
 		return b;
 	}
 
+	/** Create a horizontal box containing three of boxes.
+	 *  @param b1 is the left box
+	 *  @param b2 is the middle box
+	 *  @param b3 is the right box
+	 */
 	private Box tripleBox(Box b1, Box b2, Box b3) {
 		Box b = Box.createHorizontalBox();
 			   b.add(Box.createHorizontalStrut(10));
@@ -117,11 +129,14 @@ public class AcctMgr extends MouseAdapter implements ActionListener, FocusListen
 	 *  Setup the various user interface components.
 	 */
 	AcctMgr() {
+		// assign default values to strings used for contents of
+		// text fields
 		serverName = userName = password = "foo";
 		realName = email = defRates = totRates = "foo";
 		newPassword = "foo"; confirmPassword = "bar";
 		updateTarget = photoFile = "foo";
 
+		// setup top level
 		JFrame jfrm = new JFrame("Forest account manager");
 		jfrm.setLayout(new FlowLayout());
 		jfrm.setSize(850,400);
@@ -236,11 +251,139 @@ public class AcctMgr extends MouseAdapter implements ActionListener, FocusListen
 		setupIo();
 	}
 
+	/** Display a string in the status area.
+	 *  @param s is the string to display; it replaces any old value
+	 */
+	public void showStatus(String s) {
+		statusArea.selectAll(); statusArea.replaceSelection(s);
+	}
+
+	/** Respond to user interface events.
+	 *  ae is an action event relating to one of the user interface
+	 *  elements. Handle buttons only.
+	 */
+	public void actionPerformed(ActionEvent ae) {
+		Object src = ae.getSource();
+		if (src == connectBtn) {
+			// connect to server
+			if (connect(serverName)) {
+				showStatus("Success\nEnter user name and " +
+					   "password, then login or create " +
+					   "new account");
+			} else {
+				showStatus("Cannot connect");
+			}
+		} else if (ae.getActionCommand().equals("login")) {
+			// login to account
+			String status = login(userName,password);
+			if (status == null) {
+				showStatus("Logged in");
+			} else {
+				showStatus("Login failed: " + status);
+			}
+		} else if (ae.getActionCommand().equals("new account")) {
+			// create new account
+			String status = newAccount(userName,password);
+			if (status == null) {
+				showStatus("New account created\n" +
+					   "Update profile and change " +
+				   	   "password below");
+			} else {
+				showStatus("Error: " + status);
+			}
+		} else if (ae.getActionCommand().equals("update")) {
+			// update a user's profile
+			String status = updateProfile(updateTarget);
+			if (status == null) {
+				showStatus("Profile updated");
+			} else {
+				showStatus("Cannot update profile: " + status);
+			}
+		} else if (ae.getActionCommand().equals("change password")) {
+			// change a password
+			if (!newPassword.equals(confirmPassword)) {
+				showStatus("Mismatch. Please re-type password "+
+					  "in both new password and confirm " +
+					  "password boxes");
+			} else {
+				String status = changePassword(updateTarget,
+							       newPassword);
+				if (status == null) {
+					showStatus("Password changed");
+				} else {
+					showStatus("Error: " + status);
+				}
+			}
+		} else if (ae.getActionCommand().equals("upload photo")) {
+			// upload a photograph
+			String status = uploadPhoto(photoFile);
+			if (status == null) {
+				showStatus("Photo uploaded");
+			} else {
+				showStatus("Error: " + status);
+			}
+		}
+	}
+
+	/** Capture value of text field whenever focus leaves the field.
+	 *  @param fe is a focus event; its source is the user interface
+	 *  element that just lost focus
+	 */
+	public void focusLost(FocusEvent fe) {
+		Object src = fe.getSource();
+		if (src == serverField) {
+			serverName = serverField.getText();
+		} else if (src == userField) {
+			userName = userField.getText();
+		} else if (src == pwdField) {
+			char[] x = pwdField.getPassword();
+			password = new String(x);
+		} else if (src == nupwdField) {
+			char[] x = nupwdField.getPassword();
+			newPassword = new String(x);
+		} else if (src == confirmField) {
+			char[] x = confirmField.getPassword();
+			confirmPassword = new String(x);
+		} else if (src == nameField) {
+			realName = nameField.getText();
+		} else if (src == emailField) {
+			email = emailField.getText();
+		} else if (src == drateField) {
+			defRates = drateField.getText();
+		} else if (src == trateField) {
+			totRates = trateField.getText();
+		} else if (src == updateField) {
+			// when focus leaves updateField, retrive the profile
+			// for the specified user
+			updateTarget = updateField.getText();
+			String status = getProfile(updateTarget);
+			if (status == null) {
+				showStatus("Got target profile");
+			} else {
+				showStatus("Error: " + status);
+				updateField.setText(userName);
+				updateTarget = userName;
+			}
+		} else if (src == photoField) {
+			photoFile = photoField.getText();
+		}
+	}
+
+	public void focusGained(FocusEvent fe) {
+	}
+
+	/** Respond to mouse click in the panel by turning wall on/off.
+	 *  @param e is a mouse event corresponding to a click in the panel.
+	 */
+	public void mouseClicked(MouseEvent e) {
+	}
 	Charset ascii;
         CharsetEncoder enc;
 	CharBuffer cbuf;
 	ByteBuffer bbuf;
 
+	/** Setup to transfer ASCII characters on socket to client manager.
+	 */
 	private void setupIo() {
 		ascii = Charset.forName("US-ASCII");
                 enc = ascii.newEncoder();
@@ -250,6 +393,9 @@ public class AcctMgr extends MouseAdapter implements ActionListener, FocusListen
 		bbuf = ByteBuffer.allocate(1000);
 	}
 
+	/** Send a string to the client manager socket.
+	 *  @param s is the string to be sent; should use only ASCII characters
+	 */
 	private boolean sendString(String s) {
 		cbuf.clear(); bbuf.clear();
 		cbuf.put(s); cbuf.flip();
@@ -263,13 +409,20 @@ public class AcctMgr extends MouseAdapter implements ActionListener, FocusListen
 		return true;
 	}
 
+	/** Session information object. */
 	private class Session {
 		int clientAdr;
 		int rtrAdr;
 		RateSpec rates;
 	};
-	Session[] sessVec;
+	Session[] sessVec;	///< array of session objects
 
+	/** Show list of sessions with key parameters and cancel button.
+	 *  Complete this later. Plan is to show the sessions, one per line,
+	 *  with the client's forest address, it's routers address and a
+	 *  rate spec. Include a cancel button at the end of the line.
+	 *  When pressed, this causes the session to be terminated.
+	 */
 	private Box showSessions() {
 		JLabel sessLabel = new JLabel("sessions");
 		Box b = Box.createVerticalBox();
@@ -277,6 +430,89 @@ public class AcctMgr extends MouseAdapter implements ActionListener, FocusListen
 		return b;
 	}
 
+	/** Connect to the server for the client manager.
+	 *  @return true on success, else false
+	 */
+	private boolean connect(String server) {
+		// open channel to server
+		// on success, return true
+		try {
+			SocketAddress serverAdr = new InetSocketAddress(
+                                      InetAddress.getByName(server),CM_PORT);
+                	// connect to server
+                	serverChan = SocketChannel.open(serverAdr);
+		} catch(Exception e) {
+			return false;
+		}
+		if (!serverChan.isConnected()) return false;
+// add initial greeting message from ClientMgr so that we can
+// detect when we're really talking to server and not just tunnel
+		if (!sendString("Forest-login-v1\n")) return false;
+		inBuf = new NetBuffer(serverChan,1000);
+		return true;
+	}
+
+	/** Login to a specific account.
+	 *  @param user is the name of the user whose account we're logging into
+	 *  @param pwd is the password
+	 *  @return null if the operation succeeds, otherwise a string
+	 *  containing an error message from the client manager
+	 */
+	private String login(String user, String pwd) {
+		if (userName.length() == 0 || pwd.length() == 0)
+			return "missing user name or password";
+		if (!sendString("login: " + user + "\n" +
+			   	"password: " + pwd + "\nover\n"))
+			return "cannot send request to server";
+		String s = inBuf.readLine();
+		if (s == null || !s.equals("success")) {
+			inBuf.nextLine();
+			return s == null ? "unexpected response" : s;
+		}
+		s = inBuf.readLine();
+		if (s == null || !s.equals("over"))
+			return s == null ? "unexpected response" : s;
+
+		s = getProfile(user);
+		if (s != null) return s;
+		updateField.setText(user);
+		updateTarget = user;
+		return null;
+	}
+
+	/** Create a new account.
+	 *  @param user is the user name for the account being created
+	 *  @param pwd is the password to assign to the account
+	 *  @return null if the operation succeeds, otherwise a string
+	 *  containing an error message from the client manager
+	 */
+	private String newAccount(String user, String pwd) {
+		if (!sendString("newAccount: " + user + "\n" +
+			   	"password: " + pwd + "\nover\n"))
+			return "cannot send request to server";
+		String s = inBuf.readLine();
+		if (s == null || !s.equals("success")) {
+			inBuf.nextLine();
+			return s == null ? "unexpected response" : s;
+		}
+		s = inBuf.readLine();
+		if (s == null || !s.equals("over")) 
+			return s == null ? "unexpected response" : s;
+		s = getProfile(user);
+		if (s != null) return s;
+		updateField.setText(user);
+		updateTarget = user;
+		return null;
+	}
+
+	/** Update a client's profile.
+	 *  This method sends a request to the client manager and checks
+	 *  the response.
+	 *  @param userName is the name of the client whose profile is
+	 *  to be modified.
+	 *  @return null if the operation succeeded, otherwise,
+	 *  a string containing an error message from the client manager
+	 */
 	private String updateProfile(String userName) {
 		if (userName.length() == 0) return "empty user name";
 		if (!sendString("updateProfile: " + userName + "\n" +
@@ -295,6 +531,15 @@ public class AcctMgr extends MouseAdapter implements ActionListener, FocusListen
 		return null;
 	}
 
+	/** Change a client's password.
+	 *  This method sends a request to the client manager and checks
+	 *  the response.
+	 *  @param userName is the name of the client whose profile is
+	 *  to be modified.
+	 *  @param pwd is the new password to be assigned
+	 *  @return null if the operation succeeded, otherwise,
+	 *  a string containing an error message from the client manager
+	 */
 	private String changePassword(String userName, String pwd) {
 		if (userName.length() == 0 || pwd.length() == 0)
 			return "empty user name or password";
@@ -312,6 +557,15 @@ public class AcctMgr extends MouseAdapter implements ActionListener, FocusListen
 		return null;
 	}
 
+	/** Get a client's profile.
+	 *  This method sends a request to the client manager and processes
+	 *  the response. The text fields in the displayed profile are
+	 *  updated based on the response.
+	 *  @param userName is the name of the client whose profile is
+	 *  to be modified
+	 *  @return null if the operation succeeded, otherwise,
+	 *  a string containing an error message from the client manager
+	 */
 	private String getProfile(String userName) {
 		String s;
 		boolean gotName, gotEmail, gotDefRates, gotTotRates; 
@@ -366,178 +620,6 @@ public class AcctMgr extends MouseAdapter implements ActionListener, FocusListen
 			}
 			inBuf.nextLine();
 		}
-	}
-
-	private boolean connect(String server) {
-		// open channel to server
-		// on success, return true
-		try {
-			SocketAddress serverAdr = new InetSocketAddress(
-                                      InetAddress.getByName(server),CM_PORT);
-                	// connect to server
-                	serverChan = SocketChannel.open(serverAdr);
-		} catch(Exception e) {
-			return false;
-		}
-		if (!serverChan.isConnected()) return false;
-// add initial greeting message from ClientMgr so that we can
-// detect when we're really talking to server and not just tunnel
-		if (!sendString("Forest-login-v1\n")) return false;
-		inBuf = new NetBuffer(serverChan,1000);
-		return true;
-	}
-
-	private String login(String user, String pwd) {
-		if (userName.length() == 0 || pwd.length() == 0)
-			return "missing user name or password";
-		if (!sendString("login: " + user + "\n" +
-			   	"password: " + pwd + "\nover\n"))
-			return "cannot send request to server";
-		String s = inBuf.readLine();
-		if (s == null || !s.equals("success")) {
-			inBuf.nextLine();
-			return s == null ? "unexpected response" : s;
-		}
-		s = inBuf.readLine();
-		if (s == null || !s.equals("over"))
-			return s == null ? "unexpected response" : s;
-
-		s = getProfile(user);
-		if (s != null) return s;
-		updateField.setText(user);
-		updateTarget = user;
-		return null;
-	}
-
-	private String newAccount(String user, String pwd) {
-		if (!sendString("newAccount: " + user + "\n" +
-			   	"password: " + pwd + "\nover\n"))
-			return "cannot send request to server";
-		String s = inBuf.readLine();
-		if (s == null || !s.equals("success")) {
-			inBuf.nextLine();
-			return s == null ? "unexpected response" : s;
-		}
-		s = inBuf.readLine();
-		if (s == null || !s.equals("over")) 
-			return s == null ? "unexpected response" : s;
-		s = getProfile(user);
-		if (s != null) return s;
-		updateField.setText(user);
-		updateTarget = user;
-		return null;
-	}
-
-	public void showStatus(String s) {
-		statusArea.selectAll(); statusArea.replaceSelection(s);
-	}
-
-	/** Respond to user interface events.
-	 *  ae is an action event relating to one of the user interface
-	 *  elements. Handle buttons only.
-	 */
-	public void actionPerformed(ActionEvent ae) {
-		Object src = ae.getSource();
-		if (src == connectBtn) {
-			if (connect(serverName)) {
-				showStatus("Success\nEnter user name and " +
-					   "password, then login or create " +
-					   "new account");
-			} else {
-				showStatus("Cannot connect");
-			}
-		} else if (ae.getActionCommand().equals("login")) {
-			String status = login(userName,password);
-			if (status == null) {
-				showStatus("Logged in");
-			} else {
-				showStatus("Login failed: " + status);
-			}
-		} else if (ae.getActionCommand().equals("new account")) {
-			String status = newAccount(userName,password);
-			if (status == null) {
-				showStatus("New account created\n" +
-					   "Update profile and change " +
-				   	   "password below");
-			} else {
-				showStatus("Error: " + status);
-			}
-		} else if (ae.getActionCommand().equals("update")) {
-			String status = updateProfile(updateTarget);
-			if (status == null) {
-				showStatus("Profile updated");
-			} else {
-				showStatus("Cannot update profile: " + status);
-			}
-		} else if (ae.getActionCommand().equals("change password")) {
-			if (!newPassword.equals(confirmPassword)) {
-				showStatus("Mismatch. Please re-type password "+
-					  "in both new password and confirm " +
-					  "password boxes");
-			} else {
-				String status = changePassword(updateTarget,
-							       newPassword);
-				if (status == null) {
-					showStatus("Password changed");
-				} else {
-					showStatus("Error: " + status);
-				}
-			}
-		} else if (ae.getActionCommand().equals("upload photo")) {
-			String status = uploadPhoto(photoFile);
-			if (status == null) {
-				showStatus("Photo uploaded");
-			} else {
-				showStatus("Error: " + status);
-			}
-		}
-	}
-
-	public void focusLost(FocusEvent fe) {
-		Object src = fe.getSource();
-		if (src == serverField) {
-			serverName = serverField.getText();
-		} else if (src == userField) {
-			userName = userField.getText();
-		} else if (src == pwdField) {
-			char[] x = pwdField.getPassword();
-			password = new String(x);
-		} else if (src == nupwdField) {
-			char[] x = nupwdField.getPassword();
-			newPassword = new String(x);
-		} else if (src == confirmField) {
-			char[] x = confirmField.getPassword();
-			confirmPassword = new String(x);
-		} else if (src == nameField) {
-			realName = nameField.getText();
-		} else if (src == emailField) {
-			email = emailField.getText();
-		} else if (src == drateField) {
-			defRates = drateField.getText();
-		} else if (src == trateField) {
-			totRates = trateField.getText();
-		} else if (src == updateField) {
-			updateTarget = updateField.getText();
-			String status = getProfile(updateTarget);
-			if (status == null) {
-				showStatus("Got target profile");
-			} else {
-				showStatus("Error: " + status);
-				updateField.setText(userName);
-				updateTarget = userName;
-			}
-		} else if (src == photoField) {
-			photoFile = photoField.getText();
-		}
-	}
-
-	public void focusGained(FocusEvent fe) {
-	}
-
-	/** Respond to mouse click in the panel by turning wall on/off.
-	 *  @param e is a mouse event corresponding to a click in the panel.
-	 */
-	public void mouseClicked(MouseEvent e) {
 	}
 
 	/** Upload a photo to the client manager.
