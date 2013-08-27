96f8de370dfcd29ff6393593d3177f854a8426bf
gotapenny@gmail.com <gotapenny@gmail.com@859b52aa-9282-db45-41ff-2f5699c6ee4d>
2013-08-27 19:46:50 +0000

    added support for link, comtree, iface, and route table AND integrated comtree display

---
+++ trunk/java/control/console/ConnectionComtCtl.java
@@ -0,0 +1,186 @@
+package forest.control.console;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PushbackReader;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.CodingErrorAction;
+import java.util.TreeSet;
+
+import algoLib.misc.Util;
+import forest.common.Forest;
+import forest.common.Pair;
+import forest.common.RateSpec;
+//import forest.common.NetBuffer;
+import forest.control.ComtInfo;
+import forest.control.NetInfo;
+import forest.control.console.model.Circle;
+import forest.control.console.model.Line;
+import forest.control.console.model.Rect;
+
+public class ConnectionComtCtl {
+	
+    private final int MAXNODE    = 10000;
+    private final int MAXLINK    = 20000;
+    private final int MAXRTR     =  1000;
+    private final int MAXCOMTREE = 20000;
+	
+	private int cmPort = 30121;
+	private String cmAddr = "forest4.arl.wustl.edu";
+
+	private Charset ascii;
+	private CharsetEncoder enc;
+	private CharBuffer cb;
+	private ByteBuffer bb;
+	private SocketChannel serverChan;
+	private PushbackReader inRdr;
+	//private NetBuffer inBuf;
+	
+	private NetInfo netInfo;
+	private ComtInfo comtrees;
+	private TreeSet<Integer> comtSet;
+	
+	public ConnectionComtCtl(){
+		setupIo();
+		netInfo = new NetInfo(MAXNODE, MAXLINK, MAXRTR);
+		comtrees = new ComtInfo(MAXCOMTREE, netInfo);
+		comtSet = new TreeSet<Integer>();
+	}
+	
+	/**
+	 * Initialize a connection to Net Manager
+	 */
+	private void setupIo(){
+		ascii = Charset.forName("US-ASCII");
+		enc = ascii.newEncoder();
+		enc.onMalformedInput(CodingErrorAction.IGNORE);
+		enc.onUnmappableCharacter(CodingErrorAction.IGNORE);
+		cb = CharBuffer.allocate(1024);
+		bb = ByteBuffer.allocate(1024);
+	}
+	
+	/**
+	 * Send a string msg to Net Manager
+	 * @param  msg message
+	 * @return successful
+	 */
+	protected boolean sendString(String msg){
+		bb.clear(); cb.clear();
+		cb.put(msg); cb.flip();
+		enc.encode(cb,bb,false); bb.flip();
+		try {
+			System.out.println("SENDING: " + msg);
+			serverChan.write(bb);
+		} catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	protected boolean connectToComCtl() {
+		try {
+			SocketAddress serverAdr = new InetSocketAddress(
+												InetAddress.getByName(cmAddr), cmPort);
+			serverChan = SocketChannel.open(serverAdr);
+		} catch(Exception e) {
+			return false;
+		}
+		if (!serverChan.isConnected()) return false;
+		//inBuf = new NetBuffer(serverChan,1000);
+		return true;
+	}
+	
+	protected String getNet(){
+		if(!sendString("getNet\n")){
+			return "connot send request to server";
+		}
+		try {
+			inRdr = new PushbackReader(
+						new InputStreamReader(serverChan.socket().getInputStream()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if(!netInfo.read(inRdr)){
+			return "can't read network topology\n";
+		}
+		return null;
+	}
+	
+	protected String getComtSet(){
+		if(!sendString("getComtSet\n")) {
+			return "connot send request to server";
+		}
+		readComtSet(inRdr);
+		System.out.println(comtSet);
+		return null;
+	}
+	
+    /** Read a response to a getComtSet request.
+     *  The response consists of the string "comtSet" followed by a
+     *  list of comtree numbers, separated by commas and surrounded
+     *  by parentheses.
+     *  @return if working well, it returns null. Otherwise, error message
+     */
+    private String readComtSet(PushbackReader in) {
+    	String word = Util.readWord(in);
+        if (word == null || !word.equals("comtSet")) 
+        	return "readComtSet: unexpected response " + word + "\n";
+        if (!Util.verify(in,'(')) 
+        	return "readComtSet: expected left paren\n";
+        
+        comtSet.clear();
+        Util.MutableInt cnum = new Util.MutableInt();
+        while (true) {
+            if (!Util.readNum(in,cnum))
+            	return "readComtSet: could not read " + "expected comtree number\n";
+            comtSet.add(cnum.val);
+            if (Util.verify(in,')')) return null;
+            if (!Util.verify(in,','))
+                return "readComtSet: expected comma\n";
+        }
+    }
+    
+    protected String getComtree(int ccomt){
+    	if(!sendString("getComtree " + ccomt + "\n")){
+    		return "connot send request to server";
+    	}
+    	int recvdComt = comtrees.readComtStatus(inRdr);
+        if (recvdComt != ccomt) {
+            System.out.println("recvdComt=" + recvdComt);
+            return "received comtree info does not match request\n";
+        }
+        System.out.println(comtrees);
+        return null;
+    }
+
+	public ComtInfo getComtrees() {
+		return comtrees;
+	}
+	
+	public TreeSet<Integer> getComtTreeSet(){
+		return comtSet;
+	}
+	
+	public String getCmAddr() {
+		return cmAddr;
+	}
+
+	public void setCmAddr(String cmAddr) {
+		this.cmAddr = cmAddr;
+	}
+
+	public NetInfo getNetInfo() {
+		return netInfo;
+	}
+	
+	
+}
---
+++ trunk/java/control/console/ConnectionNetMgr.java
@@ -0,0 +1,353 @@
+package forest.control.console;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.CodingErrorAction;
+import java.util.ArrayList;
+
+import javax.swing.table.AbstractTableModel;
+
+import forest.common.NetBuffer;
+import forest.control.console.model.AdminProfile;
+import forest.control.console.model.ComtreeTable;
+import forest.control.console.model.ComtreeTableModel;
+import forest.control.console.model.IfaceTable;
+import forest.control.console.model.IfaceTableModel;
+import forest.control.console.model.LinkTable;
+import forest.control.console.model.LinkTableModel;
+import forest.control.console.model.RouteTable;
+import forest.control.console.model.RouteTableModel;
+
+public class ConnectionNetMgr {
+	private String nmAddr;
+	private int nmPort = -1;
+	
+	private Charset ascii;
+	private CharsetEncoder enc;
+	private CharBuffer cb;
+	private ByteBuffer bb;
+	private SocketChannel serverChan;
+	private NetBuffer inBuf;
+	
+	private AdminProfile adminProfile;
+	
+	public ConnectionNetMgr(){
+		adminProfile = new AdminProfile();
+		setupIo();
+	}
+	
+	/**
+	 * Initialize a connection to Net Manager
+	 */
+	private void setupIo(){
+		ascii = Charset.forName("US-ASCII");
+		enc = ascii.newEncoder();
+		enc.onMalformedInput(CodingErrorAction.IGNORE);
+		enc.onUnmappableCharacter(CodingErrorAction.IGNORE);
+		cb = CharBuffer.allocate(1024);
+		bb = ByteBuffer.allocate(1024);
+	}
+	
+	protected boolean connectToNetMgr() {
+		// open channel to server
+		// on success, return true
+		try {
+			SocketAddress serverAdr = new InetSocketAddress(InetAddress.getByName(nmAddr), nmPort);
+			// connect to server
+			serverChan = SocketChannel.open(serverAdr);
+		} catch(Exception e) {
+			return false;
+		}
+		if (!serverChan.isConnected()) return false;
+		// add initial greeting message from NetMgr so that we can
+		// detect when we're really talking to server and not just tunnel
+		if (!sendString("Forest-Console-v1\n")) return false;
+		inBuf = new NetBuffer(serverChan,1000);
+		return true;
+	}
+	
+	/**
+	 * Send a string msg to Net Manager
+	 * @param  msg message
+	 * @return successful
+	 */
+	protected boolean sendString(String msg){
+		bb.clear(); cb.clear();
+		cb.put(msg); cb.flip();
+		enc.encode(cb,bb,false); bb.flip();
+		try {
+			System.out.println("SENDING: " + msg);
+			serverChan.write(bb);
+		} catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	/** Login to a specific account.
+	 *  @param user is the name of the user whose account we're logging into
+	 *  @param pwd is the password
+	 *  @return null if the operation succeeds, otherwise a string
+	 *  containing an error message from the client manager
+	 */
+	protected String login(String user, String pwd){
+		if (user.length() == 0 || pwd.length() == 0)
+			return "missing user name or password";
+		if (!sendString("login: " + user + "\n" +
+				"password: " + pwd + "\nover\n"))
+			return "cannot send request to server";
+		String s = inBuf.readLine();
+		if (s == null || !s.equals("success")) {
+			inBuf.nextLine();
+			return s == null ? "unexpected response" : s;
+		}
+		s = inBuf.readLine();
+		if (s == null || !s.equals("over"))
+			return s == null ? "unexpected response" : s;
+
+		s = getProfile(user);
+		if (s != null) return s;
+		System.out.println(adminProfile);
+		return null;
+	}
+	
+	/** Get a admin's profile.
+	 *  This method sends a request to the net manager and processes
+	 *  the response. The text fields in the displayed profile are
+	 *  updated based on the response.
+	 *  @param userName is the name of the admin whose profile is
+	 *  to be modified
+	 *  @return null if the operation succeeded, otherwise,
+	 *  a string containing an error message from the net manager
+	 */
+	protected String getProfile(String userName) {
+		String s;
+		boolean gotName, gotEmail;
+		adminProfile.setUserName(userName);
+
+		if (userName.length() == 0) return "empty user name";
+		if (!sendString("getProfile: " + userName + "\nover\n"))
+			return "cannot send request to server";
+		
+		gotName = gotEmail = false;
+
+		while (true) {
+			s = inBuf.readAlphas();
+			if (s == null) {
+				// skip
+			} else if (s.equals("realName") && inBuf.verify(':')) {
+				String realName = inBuf.readString();
+				if (realName != null)
+					adminProfile.setRealName(realName);
+				else
+					adminProfile.setRealName("noname");
+				gotName = true;
+			} else if (s.equals("email") && inBuf.verify(':')) {
+				String email = inBuf.readWord();
+				if (email != null) 
+					adminProfile.setEmail(email);
+				else
+					adminProfile.setEmail("nomail");
+				gotEmail = true;
+			}  else if (s.equals("over")) {
+				inBuf.nextLine();
+				if (gotName && gotEmail)
+					return null;
+				else
+					return "incomplete response";
+			} else {
+				return s == null ? "unexpected response " : s;
+			}
+			inBuf.nextLine();
+		}
+	}
+
+	/** Create a new account.
+	 *  @param user is the user name for the account being created
+	 *  @param pwd is the password to assign to the account
+	 *  @return null if the operation succeeds, otherwise a string
+	 *  containing an error message from the net manager
+	 */
+	protected String newAccount(String user, String pwd) {
+		if (!sendString("newAccount: " + user + "\n" +
+				"password: " + pwd + "\nover\n"))
+			return "cannot send request to server";
+		String s = inBuf.readLine();
+		if (s == null || !s.equals("success")) {
+			inBuf.nextLine();
+			return s == null ? "unexpected response" : s;
+		}
+		s = inBuf.readLine();
+		if (s == null || !s.equals("over")) 
+			return s == null ? "unexpected response" : s;
+
+		return null;
+	}
+
+	/** Update a admin's profile.
+	 *  This method sends a request to the net manager and checks
+	 *  the response.
+	 *  @param userName is the name of the admin whose profile is
+	 *  to be modified.
+	 *  @return null if the operation succeeded, otherwise,
+	 *  a string containing an error message from the net manager
+	 */
+	protected String updateProfile(String userName, String realName, String email) {
+		if (userName.length() == 0) return "empty user name";
+		if (!sendString("updateProfile: " + userName + "\n" +
+			   	"realName: \"" + realName + "\"\n" +
+				"email: " + email + "\n" + "over\n"))
+			return "cannot send request";
+		String s = inBuf.readLine();
+		if (s == null || !s.equals("success")) {
+			inBuf.nextLine(); return s;
+		}
+		s = inBuf.readLine();
+		if (s == null || !s.equals("over")) return s;
+		return null;
+	}
+	
+	/** Change an admin's password.
+	 *  This method sends a request to the net manager and checks
+	 *  the response.
+	 *  @param userName is the name of the admin whose profile is
+	 *  to be modified.
+	 *  @param pwd is the new password to be assigned
+	 *  @return null if the operation succeeded, otherwise,
+	 *  a string containing an error message from the net manager
+	 */
+	protected String changePassword(String userName, String pwd) {
+		if (userName.length() == 0 || pwd.length() == 0)
+			return "empty user name or password";
+		if (!sendString("changePassword: " + userName + " "
+				+ pwd + "\nover\n"))
+			return "cannot send request to server";
+		String s = inBuf.readLine();
+		if (s == null || !s.equals("success")) {
+			inBuf.nextLine(); 
+			return s == null ? "unexpected response " : s;
+		}
+		s = inBuf.readLine();
+		if (s == null || !s.equals("over"))
+			return s == null ? "unexpected response " : s;
+		return null;
+	}
+	
+	/**
+	 * Retrieve router table info(link, comtree, iface, route) and put them into 
+	 * AbstractTableModel class
+	 * @param tableModel TableModel for each router info
+	 * @param routerName Router Name such as r1
+	 * @return null if successful, otherwise, an error message
+	 */
+	protected String getTable(AbstractTableModel tableModel, String routerName){
+		String type = null;
+		if(tableModel instanceof ComtreeTableModel){
+			type = "Comtree";
+			((ComtreeTableModel) tableModel).clear();
+		} else if(tableModel instanceof LinkTableModel){
+			type = "Link";
+			((LinkTableModel) tableModel).clear();
+		} else if(tableModel instanceof IfaceTableModel){
+			type = "Iface";
+			((IfaceTableModel) tableModel).clear();
+		} else if(tableModel instanceof RouteTableModel){
+			type = "Route";
+			((RouteTableModel) tableModel).clear();
+		} else {
+			return "no type defined";
+		}
+		String sendMsg = "get" + type + "Table: ";
+		if(!sendString(sendMsg + routerName + "\n")){
+			return "connot send request to server";
+		}
+		ArrayList<String> lines = new ArrayList<String>();
+		while(true){
+			String str = inBuf.readLine();
+			System.out.println(str);
+			if(str.equals("over")){
+				if(tableModel instanceof ComtreeTableModel){
+					for(String s : lines){
+						s = s.replaceAll("\\s+", " ");
+						String[] tokens = s.split(" ");
+						if(tokens.length == 6){
+							((ComtreeTableModel) tableModel).addComtreeTable(new ComtreeTable(
+									tokens[1], tokens[2], tokens[3], tokens[4], tokens[5]));
+						} else if (tokens.length == 5){
+							((ComtreeTableModel) tableModel).addComtreeTable(new ComtreeTable(
+									tokens[1], tokens[2], tokens[3], " ", tokens[4]));
+						} else {
+							return "tokens error";
+						}
+					}
+				} else if(tableModel instanceof LinkTableModel){
+					for(String s : lines){
+						s = s.replaceAll("\\s+", " ");
+						String[] tokens = s.split(" ");
+						if(tokens.length == 9){
+							((LinkTableModel) tableModel).addLinkTable(new LinkTable(tokens[1], 
+										tokens[2], tokens[3], tokens[4], tokens[5], tokens[6]));
+						} else {
+							return "tokens error";
+						}
+					}
+				} else if(tableModel instanceof IfaceTableModel){
+					for(String s : lines){
+						s = s.replaceAll("\\s+", " ");
+						String[] tokens = s.split(" ");
+						if(tokens.length == 4){
+							((IfaceTableModel) tableModel).addIfaceTable(new IfaceTable(tokens[1], 
+									tokens[2], tokens[3]));
+						} else {
+							return "tokens error";
+						}
+					}
+				} else if(tableModel instanceof RouteTableModel){
+					for(String s : lines){
+						String[] tokens = s.split(" ");
+						if(tokens.length == 3){
+							((RouteTableModel) tableModel).addRouteTable(new RouteTable(tokens[0], 
+																		tokens[1], tokens[2]));
+						} else {
+							return "tokens error";
+						}
+					}
+				}
+				break;
+			} else  if(str.equals("success")){
+				continue;
+			} else {
+				lines.add(str);
+			}
+		}
+		tableModel.fireTableDataChanged();
+		return null;
+	}
+		
+	public String getNmAddr() {
+		return nmAddr;
+	}
+
+	public void setNmAddr(String nmAddr) {
+		this.nmAddr = nmAddr;
+	}
+
+	public int getNmPort() {
+		return nmPort;
+	}
+
+	public void setNmPort(int nmPort) {
+		this.nmPort = nmPort;
+	}
+
+	public AdminProfile getAdminProfile() {
+		return adminProfile;
+	}
+}
