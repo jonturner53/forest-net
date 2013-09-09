10b7425700277b93e1ca302a20d92773889754f9
jonturneratwustl@gmail.com <jonturneratwustl@gmail.com@859b52aa-9282-db45-41ff-2f5699c6ee4d>
2013-09-09 18:11:12 +0000

    added packet filtering

---
+++ cpp/common/PacketFilter.cpp
@@ -0,0 +1,48 @@
+/** @file PacketFilter.cpp 
+ *
+ *  @author Jon Turner
+ *  @date 2011
+ *  This is open source software licensed under the Apache 2.0 license.
+ *  See http://www.apache.org/licenses/LICENSE-2.0 for details.
+ */
+
+#include "PacketFilter.h"
+
+namespace forest {
+
+/** Constructor for PacketFilter, allocates space and initializes private data.
+ */
+PacketFilter::PacketFilter() { on = false; }
+
+/** Create a string representation of a filter.
+ *  @param s is a reference to a string in which result is to be returned
+ *  @return a reference to s
+ */
+string& PacketFilter::toString(string& s) const {
+	stringstream ss;
+	ss << (on ? 1 : 0 ) << " " << lnk << " "
+	   << (in ? 1 : 0) << (out ? 1 : 0) << " ";
+	ss << comt << " ";
+	ss << Forest::fAdr2string(srcAdr,s) << " ";
+	ss << Forest::fAdr2string(dstAdr,s) << " ";
+	ss << Packet::pktTyp2string(type,s);
+	ss << CtlPkt::cpType2string(cpType,s);
+	s = ss.str();
+	return s;
+}
+
+bool PacketFilter::fromString(string& s) {
+	NetBuffer buf(s); string s1, s2;
+	return	(buf.readBit(on) && buf.readBit(in) && buf.readBit(out) &&
+		 buf.readInt(comt) &&
+		 buf.readForestAddress(s1) &&
+		 (srcAdr = Forest::forestAdr(s1.c_str())) != 0 &&
+		 buf.readForestAddress(s2) &&
+		 (dstAdr = Forest::forestAdr(s2.c_str())) != 0 &&
+		 buf.readPktType(type) &&
+		 buf.readCpType(cpType));
+}
+		
+
+} // ends namespace
+
--- cpp/common/PacketLog.cpp
+++ cpp/common/PacketLog.cpp
@@ -10,64 +10,111 @@
 
 namespace forest {
 
-
 /** Constructor for PacketLog, allocates space and initializes private data.
  */
-PacketLog::PacketLog(int maxPkts1, int maxData1, PacketStore *ps1)
-	  	     : maxPkts(maxPkts1), maxData(maxData1), ps(ps1) {
-	events = new EventStruct[maxPkts];
-	numPkts = numData = 0;
-	dumpTime = 0;
+PacketLog::PacketLog(int maxEvents1, int maxFilters1, PacketStore *ps1)
+	  	     : maxEvents(maxEvents1), maxFilters(maxFilters1), ps(ps1) {
+	evec = new EventStruct[maxEvents];
+	eventCount = firstEvent = lastEvent = 0;
+	fvec = new PacketFilter[maxFilters+1];
+	filters = new UiSetPair(maxFilters);
 }
 
 /** Destructor for PacketLog, deletes allocated space */
-PacketLog::~PacketLog() { delete [] events; }
+PacketLog::~PacketLog() { delete [] evec; delete [] fvec; delete filters; }
 
-/** Log a packet.
+/** Log a packet if it matches a stored filter.
  *  @param p is the number of the packet to be logged
  *  @param lnk is the link the packet is being sent on (or was received from)
  *  @param sendFlag is true if the packet is being sent; else it is false
  *  @param now is the time at which the packet is being sent/received
+ *  Received packet is compared to all enabled filters. If it matches
+ *  any filter, a copy is made and saved in the log. If the packet
+ *  can't be saved, we record a "gap" in the log. If several packets
+ *  in a row cannot be logged, the number of missing packets is
+ *  recorded as part of the gap record.
  */
 void PacketLog::log(pktx px, int lnk, bool sendFlag, uint64_t now) {
-        Packet& p = ps->getPacket(px);
-        if (numPkts < maxPkts &&
-            (p.type != Forest::CLIENT_DATA || numData <= maxData)) {
-                int px1 = (p.type == Forest::CLIENT_DATA ?
-                          ps->clone(px) : ps->fullCopy(px));
-                events[numPkts].px = px1;
-                events[numPkts].sendFlag = sendFlag;
-                events[numPkts].link = lnk;
-                events[numPkts].time = now;
-                numPkts++;
-                if (p.type == Forest::CLIENT_DATA) numData++;
-        }
-	if (now < dumpTime + 1000000000) return;
-	dumpTime = now;
-	string fname = "packetLogSwitch";
-	ifstream fs; fs.open(fname.c_str());
-	string plSwitch;
-	if (fs.fail()) return;
-        if (Misc::readWord(fs,plSwitch) && plSwitch == "on")
-		write(cout);
-	fs.close();
-	numPkts = 0;
+	if (firstFilter() == 0) return;
+	for (fltx f = firstFilter(); f != 0; f = nextFilter(f)) {
+		if (match(f,px,lnk,sendFlag)) break;
+		if (nextFilter(f) == 0) return; // no filters match
+	}
+	// reach here if packet matched some filter
+	// make a record in event vector
+	int px1 = ps->fullCopy(px);
+	if (px1 == 0 || eventCount == maxEvents) { // record gap
+		if (px1 != 0) ps->free(px1);
+		// use px == 0 to indicate gap in sequence
+		// for these records, link field is used to record
+		// number of packets that were missed (size of gap)
+		if (eventCount > 0 && evec[lastEvent].px == 0) {
+			// existing gap record
+			evec[lastEvent].link++; return;
+		}
+		// need to convert last record to a gap record
+		evec[lastEvent].px = 0;
+		evec[lastEvent].link = 1;
+		evec[lastEvent].time = now;
+		if (eventCount == 0) eventCount++;
+		return;
+	}
+	// common case - just add new record for packet
+	if (eventCount > 0)
+		if (++lastEvent >= maxEvents) lastEvent = 0;
+	eventCount++;
+	evec[lastEvent].px = px1;
+	evec[lastEvent].sendFlag = sendFlag;
+	evec[lastEvent].link = lnk;
+	evec[lastEvent].time = now;
+	return;
 }
 
-/** Write all logged packets.
- *  @param out is an open output stream
+/** Extract event records from the log.
+ *  @param maxLen is the maximum number of characters to return
+ *  @param s is a reference to a string in which result is returned
+ *  @return the number of log events in the returned string.
+ *  Events that are copied to buf are removed from the log.
  */
-void PacketLog::write(ostream& out) const {
-	for (int i = 0; i < numPkts; i++) {
-		string s;
-		if (events[i].sendFlag) out << "send link ";
-		else 			out << "recv link ";
-		out << setw(2) << events[i].link << " at "
-		     << Misc::nstime2string(events[i].time,s) << " ";
-		pktx px = events[i].px;
-		out << ps->getPacket(px).toString(s);
+int PacketLog::log2string(int maxLen, string& s) {
+	stringstream ss;
+	int count = 0; s = "";
+	while (eventCount > 0) {
+		ss.str("");
+		int px = evec[firstEvent].px;
+		ss << Misc::nstime2string(evec[firstEvent].time,s);
+		if (px == 0) {
+			ss << " missing " << evec[firstEvent].link << endl;
+		} else {
+			if (evec[firstEvent].sendFlag)	ss << "send ";
+			else				ss << "recv ";
+			ss << "link " << setw(2) << evec[firstEvent].link;
+			ss << " " << ps->getPacket(px).toString(s);
+		}
+		if (s.length() + ss.str().length() > maxLen) break;
+		s += ss.str();
+		ps->free(px);
+		count++; eventCount--;
+		if (++firstEvent >= maxEvents) firstEvent = 0;
 	}
-	out.flush();
+	return count;
+}
+
+/** Add a new filter to the table of filters.
+ *  The filter is disabled initially
+ *  @return the filter number of the new filter or 0, if could not add
+ */
+int PacketLog::addFilter() {
+	fltx f = filters->firstOut();
+	if (f == 0) return 0;
+	filters->swap(f); disable(f);
+	return f;
+}
+
+/** Remves a filter from the table.  */
+void PacketLog::dropFilter(fltx f) {
+	if (!filters->isIn(f)) return;
+	disable(f); filters->swap(f);
 }
 
 } // ends namespace
