c867e22b38e87698c339024541a1230cbac42acb
gotapenny@gmail.com <gotapenny@gmail.com@859b52aa-9282-db45-41ff-2f5699c6ee4d>
2013-09-13 19:42:47 +0000

    added comtree rate on the comtree display

--- java/control/console/model/Line.java
+++ java/control/console/model/Line.java
@@ -7,14 +7,15 @@ public class Line{
     private boolean strong = false;
     private RateSpec rs;
     private RateSpec availableRs;
-//    private RateSpec ctRs;
+    private RateSpec comtreeRs;
     public Line(int lx, int ly, int rx, int ry, boolean strong, 
-    		RateSpec rs, RateSpec availableRs){
+    		RateSpec rs, RateSpec availableRs, RateSpec comtreeRs){
         this.lx = lx; this.ly = ly;
         this.rx = rx; this.ry = ry;
         this.strong = strong;
-        this.rs = rs; //this.ctRs = ctRs;
+        this.rs = rs;
         this.availableRs = availableRs;
+        this.comtreeRs = comtreeRs;
     }
     public int getLx(){ return lx;}
     public int getLy(){ return ly;}
@@ -23,5 +24,5 @@ public class Line{
     public boolean isStrong(){return strong;}
     public RateSpec getRateSpec(){return rs;}
     public RateSpec getAvailableRS(){return availableRs;}
-//    public RateSpec getCtRs(){return ctRs;}
+    public RateSpec getComtreeRs(){return comtreeRs;}
 }
