c0f41197543c59b165bbf26a4d8721ad07aa8586
jonturneratwustl@gmail.com <jonturneratwustl@gmail.com@859b52aa-9282-db45-41ff-2f5699c6ee4d>
2014-08-14 14:40:23 +0000

    modified NetMgr to use new ComtCtl

--- cpp/control/Substrate.cpp
+++ cpp/control/Substrate.cpp
@@ -10,51 +10,33 @@
 
 namespace forest {
 
-Substrate::Substrate(fAdr_t myAdr1, ipa_t myIp1, fAdr_t rtrAdr1, ipa_t rtrIp1,
-		ipp_t rtrPort1, uint64_t nonce1, int threadCount1,
-		void* (*handler1)(void*), int dgPort1, int listenPort1,
-		PacketStoreTs *ps1, Logger *logger1)
-		: myAdr(myAdr1), myIp(myIp1), rtrAdr(rtrAdr1), rtrIp(rtrIp1),
-		  rtrPort(rtrPort1), nonce(nonce1), threadCount(threadCount1),
-		  handler(handler1), dgPort(dgPort1),
-		  listenPort(listenPort1), ps(ps1), logger(logger1) {
-	pool = new ThreadInfo[threadCount+1];
-	threads = new UiSetPair(threadCount);
-	inReqMap = new IdMap(threadCount);
-	outReqMap = new IdMap(threadCount);
+Substrate::Substrate(int threadCount1, Controller *ctlr1,
+		     PacketStore *ps1, Logger *logger1) {
+	threadCount = threadCount1;
+	ctlr = ctlr1; ps = ps1; logger = logger1;
+
+	thredIdx = new ListPair(threadCount);
+	rptr = new Repeater(threadCount);
+	repH = new RepeatHandler(20*threadCount);
 
 	rtrReady = false;
+	dgSock = listenSock = -1;
 }
 
 Substrate::~Substrate() {
 	if (dgSock > 0) close(dgSock);
 	if (listenSock > 0) close(listenSock);
-	delete [] pool; delete threads; delete inReqMap; delete outReqMap;
+	delete thredIdx; delete rptr; delete repH;
 }
 
-bool Substrate::init() {
-	// setup thread pool for handling control packets
-	pthread_attr_t attr;
-	pthread_attr_init(&attr);
-	pthread_attr_setstacksize(&attr,4*PTHREAD_STACK_MIN);
-	size_t stacksize;
-	pthread_attr_getstacksize(&attr,&stacksize);
-	if (stacksize != 4*PTHREAD_STACK_MIN)
-		logger->log("Substrate:init: can't set stack size",4);
-	for (int t = 1; t <= threadCount; t++) {
-		if (!pool[t].qp.in.init() || !pool[t].qp.out.init()) {
-			logger->log("Substrate::init: cannot initialize "
-			   	   "thread queues",2);
-			return false;
-		}
-		if (pthread_create(&pool[t].thid,&attr,handler,
-				   (void *) &pool[t].qp) != 0) {
-			logger->log("Substrate::init: cannot create "
-			   	   "thread pool",2);
-			return false;
-		}
-	}
-
+bool Substrate::init(fAdr_t myAdr1, ipa_t myIp1, fAdr_t rtrAdr1,
+		     ipa_t rtrIp1, ipp_t rtrPort1, uint64_t nonce1,
+		     int dgPort1, int listenPort1, int finTime1) {
+	myAdr = myAdr1; myIp = myIp1;
+	rtrAdr = rtrAdr1; rtrIp = rtrIp1; rtrPort = rtrPort1;
+	nonce = nonce1; dgPort = dgPort1; listenPort = listenPort1;
+	finTime = finTime1;
+	
 	// setup sockets
 	dgSock = Np4d::datagramSocket();
 	if (dgSock < 0 || !Np4d::bind4d(dgSock,myIp,dgPort) ||
@@ -68,68 +50,64 @@ bool Substrate::init() {
 }
 
 /** Run forever, or until time expires.
- *  @param finTime is the number of seconds to run before halting;
- *  if zero, run forever
  */
-bool Substrate::run(int finTimeSec) {
+bool Substrate::run() {
 	seqNum = 1;
-	now = Misc::getTimeNs();
-	uint64_t finishTime = finTimeSec;
+	tZero = high_resolution_clock::now();
+	now = 0;
+	nanoseconds temp;
+
+	uint64_t finishTime = finTime;
 	finishTime *= 1000000000; // convert to ns
 
 	bool nothing2do; bool connected = false;
 	while (finishTime == 0 || now <= finishTime) {
+		temp = high_resolution_clock::now() - tZero;
+		now = temp.count();
+
 		nothing2do = true;
 		if (!connected && rtrReady) {
 			// this allows substrate to run before its
 			// access router has booted; used by NetMgr
-			if (!connect()) {
-				return false;
-			}
+			if (!connect()) return false;
 			connected = true;
 		}
-		// check for connection requests from remote clients
+		// check for connection requests from remote hosts
 		int connSock = -1;
 		if ((connSock = Np4d::accept4d(listenSock)) > 0) {
 			// let handler know this is socket# for remote host
-			int t = threads->firstOut();
-			if (t != 0) {
-				threads->swap(t);
-				pool[t].qp.in.enq(-connSock);
+			int thx = thredIdx->firstOut();
+			if (thx != 0) {
+				thredIdx->swap(thx);
+				pool[t].inq.enq(-connSock);
 			} else {
 				logger->log("Substrate: thread pool is "
 					   "exhausted",4);
 			}
 		} 
-		// check for packets from the Forest net
+		// handle incoming packets
 		pktx px = recvFromForest();
 		if (px != 0) {
 			inbound(px); nothing2do = false;
 		}
 
-		// now handle outgoing packets from the thread pool
-// can we avoid iteration? Simplest way to do this is to have threads
-// share the outgoing queue, and include thread number with packet number.
-		for (int t = threads->firstIn(); t!=0; t = threads->nextIn(t)) {
-			if (!pool[t].qp.out.empty()) {
-				outbound(pool[t].qp.out.deq(),t);
-				nothing2do = false;
-			}
+		// and outgoing packets
+		if (!retQ.empty()) {
+			pair<int,int> pp = retQ.deq();
+			outbound(pp.first,pp.second);
+			nothing2do = false;
 		}
 
-		// check for expired timeouts
-// a heap based on timestamp would be better, although
-// this is ok for the common case of a few worker threads
-		for (int t = threads->firstIn(); t != 0;
-			 t = threads->nextIn(t)) {
-			if (pool[t].seqNum != 0 && pool[t].ts < now) {
-				outReqMap->dropPair(pool[t].seqNum);
-				pool[t].seqNum = 0;
+		if (nothing2do) {
+			// check for expired entries in repH & remove oldest
+			int ox = repH.expired(now);
+			if (ox != 0) {
+				ps->free(ox); didNothing = false;
 			}
+			nothing2do = false;
 		}
-		if (nothing2do && threads->firstIn() == 0) usleep(1000);
-		sched_yield();
-		now = Misc::getTimeNs();
+		if (nothing2do && thredIdx->firstIn() == 0)
+			this_thread::sleep_for(milliseconds(1));
 	}
 	if (connected) return disconnect();
 	return true;
@@ -140,86 +118,85 @@ bool Substrate::run(int finTimeSec) {
  */
 void Substrate::inbound(pktx px) {
 	Packet& p = ps->getPacket(px);
-	if (p.type == Forest::CLIENT_SIG || p.type == Forest::NET_SIG) {
-		CtlPkt cp(p);
-		if (cp.mode == CtlPkt::REQUEST) {
-			int t = threads->firstOut();
-			uint64_t key = p.srcAdr;
-			key <<= 32; key += cp.seqNum;
-			if (inReqMap->validKey(key)) {
-				// in this case, we've got an active thread 
-				// handling this request, so ignore duplicate
-			} else if (t != 0) { // assign thread
-				threads->swap(t);
-				inReqMap->addPair(key,t);
-				pool[t].seqNum = 0;
-				pool[t].qp.in.enq(px);
-				return;
-			} else {
-				logger->log("Substrate: thread pool is "
-					   "exhausted",4);
-			}
-		} else {
-			// replies are returned to the thread that
-			// sent the request - outReqMap contains thread index
-			int t = outReqMap->getId(cp.seqNum);
-			if (t != 0) {
-				outReqMap->dropPair(cp.seqNum);
-				pool[t].seqNum = 0;
-				pool[t].qp.in.enq(px);
-				return;
-			}
-		}
+	if (p.type != Forest::CLIENT_SIG && p.type != Forest::NET_SIG) {
+		ps->free(px); return;
 	}
-	ps->free(px);
+	CtlPkt cp(p);
+	if (cp.mode == CtlPkt::REQUEST) {
+		if ((pktx sx = repH->find(p.srcAdr,cp.seqNum)) != 0) {
+			// repeat of a request we've already received
+			ps->free(px);
+			Packet& saved = ps->getPacket(sx);
+			CtlPkt scp(saved);
+			if (scp.mode != CtlPkt::REQUEST) {
+				// already replied, reply again
+				pktx cx = ps->clone(sx);
+				if (cx != 0) sendToForest(cx);
+			}
+			return;
+		}
+		// new request, save a copy
+		pktx cx = ps->clone(px);
+		pktx ox = repH.saveReq(cx,p.srcAdr,cp.seqNum,now);
+		if (ox != 0) { // old entry was removed to make room
+			ps->free(ox);
+		}
+
+		// assign a thread to handle it
+		int thx = thredIdx->firstOut();
+		if (thx != 0) { // assign thread
+			thredIdx->swap(thx);
+			pool[thx].inQ.enq(px);
+		} else {
+			logger->log("Substrate: thread pool is "
+				    "exhausted",4);
+		}
+		return;
+	}
+	// reply; drop matching request
+	pair<int,int> pp = rptr.deleteMatch(p.srcAdr,cp.seqNum);
+	if (pp.first == 0) { // no matching request
+		ps->free(px); return;
+	}
+	ps->free(pp->first); // free saved copy of request
+	// forward reply to the thread that sent the request
+	if (pp.second != 0) pool[pp.second].inq.enq(px);
+	return;
 }
 
 /** Handle an outbound packet from one of the worker threads.
  *  @param px is a packet index
- *  @param t is the thread index (in the tread pool)
+ *  @param thx is the thread index (in the tread pool)
  */
-void Substrate::outbound(pktx px, int t) {
+void Substrate::outbound(pktx px, int thx) {
 	if (px == 0) { // means worker thread completed task
-		inReqMap->dropPair(inReqMap->getKey(t));
-		pool[t].qp.in.reset();
-		threads->swap(t);
+		pool[thx].inq.reset();
+		thredIdx->swap(thx);
 		return;
 	}
 	Packet& p = ps->getPacket(px);
-	CtlPkt cp(p.payload(),p.length-Forest::OVERHEAD);
-	cp.unpack();
+	CtlPkt cp(p);
 	if (cp.mode != CtlPkt::REQUEST) {
-		// just send it and return
+		// save reply in repH, and free saved copy of request
+		pktx cx = ps->clone(px);
+		if (cx != 0) {
+			pktx sx = saveRep(cx, p.dstAdr, cp.seqNum);
+			if (sx != 0) ps->free(sx);
+		}
+		// and send reply
 		sendToForest(px); return;
 	}
-	if (cp.seqNum == 1) {
-		// means this is a repeat of a pending
-		// outgoing request
-		if (outReqMap->validId(t)) {
-			cp.seqNum = outReqMap->getKey(t);
-		} else {
-			// in this case, reply has arrived but was not yet
-			// seen by thread so, suppress duplicate request
-			ps->free(px); return;
-		}
-	} else {
-		// first time for this request, assign seq number and
-		// remember it so we can route reply to correct thread
-		if (outReqMap->validId(t))
-			outReqMap->dropPair(outReqMap->getKey(t));
-		outReqMap->addPair(seqNum,t);
-		pool[t].seqNum = seqNum;
-		cp.seqNum = seqNum++;
-// consider using timestamp to repeat outgoing requests,
-// so worker thread does not need to
-// requires heap to keep track of pending requests
-// will need to make Dheap auto-expandable
+	// request packet; remember it so we can route reply to correct thread
+	int cx = ps->clone(px);
+	if (cx == 0) {
+		// to make this robust, return NO_REPLY to thread
+		return;
 	}
-	cp.pack();
-	p.payErrUpdate();
-	// timeout used to purge old entries
-	pool[t].ts = now + 2000000000; // 2 sec timeout
+	// assign sequence number to outgoing request, send it and save copy
+	cp.seqNum = seqNum++;
+	cp.pack(); p.payErrUpdate();
 	sendToForest(px);
+	rptr->saveReq(cx, cp.seqNum, now, thx);
 }
 
 /** Check for next packet from the Forest network.
@@ -238,9 +215,8 @@ pktx Substrate::recvFromForest() {
 	}
 	p.tunIp = srcIp; p.tunPort = srcPort;
 /*
-string s;
-cerr << "got packet from " << Np4d::ip2string(srcIp,s) << " " << srcPort << endl;
-cerr << p.toString(s) << endl;
+cerr << "got packet from " << Np4d::ip2string(srcIp) << " " << srcPort << endl;
+cerr << p.toString() << endl;
 */
 
 	return px;
@@ -260,9 +236,8 @@ void Substrate::sendToForest(pktx px) {
 		ip = rtrIp; port = rtrPort;
 	}
 /*
-string s;
-cerr << "substrate sending to " << Np4d::ip2string(ip,s) << " " << port << endl;
-cerr << p.toString(s) << endl;
+cerr << "substrate sending to " << Np4d::ip2string(ip) << " " << port << endl;
+cerr << p.toString() << endl;
 */
 	if (port == 0) fatal("Substrate::sendToForest: zero port number");
 	int rv = Np4d::sendto4d(dgSock,(void *) p.buffer, p.length,ip,port);
@@ -277,23 +252,28 @@ bool Substrate::connect() {
 	pktx px = ps->alloc();
 	Packet& p = ps->getPacket(px);
 
-	p.payload()[0] = htonl((uint32_t) (nonce >> 32));
-	p.payload()[1] = htonl((uint32_t) (nonce & 0xffffffff));
-	p.length = Forest::OVERHEAD + 8; p.type = Forest::CONNECT; p.flags = 0;
+	Forest::pack64(seqNum++, p.payload());
+	Forest::pack64(nonce, p.payload()+2);
+
+	p.length = Forest::OVERHEAD + 2*sizeof(int64_t);
+	p.type = Forest::CONNECT; p.flags = 0;
 	p.comtree = Forest::CONNECT_COMT;
 	p.srcAdr = myAdr; p.dstAdr = rtrAdr;
 
-	int resendTime = Misc::getTime();
+	nanoseconds temp;
+	temp = high_resolution_clock::now() - tZero;
+	int64_t resendTime = temp.count();
 	int resendCount = 1;
 	while (true) {
-		int now = Misc::getTime();
+		temp = high_resolution_clock::now() - tZero;
+		int64_t now = temp.count();
 		if (now > resendTime) {
 			if (resendCount > 3) { ps->free(px); return false; }
 			sendToForest(px);
-			resendTime += 1000000;
+			resendTime += 1000000000;
 			resendCount++;
 		}
-		usleep(10000);
+		this_thread::sleep_for(milliseconds(10));
 		int rx = recvFromForest();
 		if (rx != 0) {
 			Packet& reply = ps->getPacket(rx);
@@ -319,17 +299,20 @@ bool Substrate::disconnect() {
 	p.flags = 0; p.comtree = Forest::CONNECT_COMT;
 	p.srcAdr = myAdr; p.dstAdr = rtrAdr;
 
-	int resendTime = Misc::getTime();
+	nanoseconds temp;
+	temp = high_resolution_clock::now() - tZero;
+	int64_t resendTime = temp.count();
 	int resendCount = 1;
 	while (true) {
-		int now = Misc::getTime();
+		temp = high_resolution_clock::now() - tZero;
+		int64_t now = temp.count();
 		if (now > resendTime) {
 			if (resendCount > 3) { ps->free(px); return false; }
 			sendToForest(px);
-			resendTime += 1000000;
+			resendTime += 1000000000;
 			resendCount++;
 		}
-		usleep(10000);
+		this_thread::sleep_for(milliseconds(100));
 		int rx = recvFromForest();
 		if (rx != 0) {
 			Packet& reply = ps->getPacket(rx);
